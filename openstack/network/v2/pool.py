a8e05de08fae318971afccef2390c944c2289dc7
Terry Howe <terrylhowe@gmail.com>
2014-07-31 14:44:37 -0600

    network/v2 load balancer pool resource
    Change-Id: I8fe094f62f4373d2dd32fa4cf86e9b89c4c363ca

---
+++ openstack/network/v2/pool.py
@@ -0,0 +1,40 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.network import network_service
+from openstack import resource
+
+
+class Pool(resource.Resource):
+    resource_key = 'pool'
+    resources_key = 'pools'
+    base_path = '/v2.0/pools'
+    service = network_service.NetworkService()
+
+    # capabilities
+    allow_create = True
+    allow_retrieve = True
+    allow_update = True
+    allow_delete = True
+    allow_list = True
+
+    # Properties
+    admin_state_up = resource.prop('admin_state_up', type=bool)
+    description = resource.prop('description')
+    healthmonitor_id = resource.prop('healthmonitor_id')
+    lb_algorithm = resource.prop('lb_algorithm')
+    members = resource.prop('members')
+    name = resource.prop('name')
+    project_id = resource.prop('tenant_id')
+    protocol = resource.prop('protocol')
+    session_persistence = resource.prop('session_persistence')
+    status = resource.prop('status')
