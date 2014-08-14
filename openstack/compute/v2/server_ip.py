b9cc37627113cc766e9ee85c603dcb67499431fe
Terry Howe <terrylhowe@gmail.com>
2014-08-14 14:33:08 -0600

    Server IP resource
    The server IP resource can only list.  The Resource.list method
    needed to be overridden because:
    * The url path contains the server_id.
    * The output is a dict of networks rather than an array.
    
    Also in this change:
    * ServerIP objects have sufficient information to stand on their
      own because they contain the server_id and network_label.
    * An ips(session) convenience method has been added to the Server.
    * The list example has been updated to pass data to path_args so
      the list method and format the url.
    
    The path_args parameter to list may be convenient for other
    classes.  I didn't add it to the Resource.list method because
    that isn't required for this change.
    
    Change-Id: Iac77aba144d2e5b4ca4cdbf97e534d83519066ef

---
+++ openstack/compute/v2/server_ip.py
@@ -0,0 +1,56 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import six
+
+from openstack.compute import compute_service
+from openstack import resource
+
+
+class ServerIP(resource.Resource):
+    resource_key = 'server_ip'
+    resources_key = 'server_ips'
+    base_path = '/servers/%(server_id)s/ips'
+    service = compute_service.ComputeService()
+
+    # capabilities
+    allow_list = True
+
+    # Properties
+    addr = resource.prop('addr')
+    network_label = resource.prop('network_label')
+    server_id = resource.prop('server_id')
+    version = resource.prop('version')
+
+    @property
+    def id(self):
+        try:
+            val = self.addr
+        except AttributeError:
+            val = None
+        return val
+
+    @classmethod
+    def list(cls, session, path_args=None, **params):
+        url = cls.base_path % path_args
+        resp = session.get(url, service=cls.service, params=params)
+        ray = []
+        for network_label, addresses in six.iteritems(resp.body['addresses']):
+            for address in addresses:
+                record = {
+                    'server_id': path_args['server_id'],
+                    'network_label': network_label,
+                    'version': address['version'],
+                    'addr': address['addr'],
+                }
+                ray.append(cls.existing(**record))
+        return ray
