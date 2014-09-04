d666be287c37aa741c05401ea071515555f96bd7
Steve Lewis <stevelle@gmail.com>
2014-09-04 13:49:51 -0700

    Adding database flavor support
    Add support for flavor's list and retrieve operations.
    
    Change-Id: I0eedc7f8aba66b0ea14c5014fc796b4e2fde347f

---
+++ openstack/database/v1/flavor.py
@@ -0,0 +1,30 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.database import database_service
+from openstack import resource
+
+
+class Flavor(resource.Resource):
+    resource_key = 'flavor'
+    resources_key = 'flavors'
+    base_path = '/flavors'
+    service = database_service.DatabaseService()
+
+    # capabilities
+    allow_list = True
+    allow_retrieve = True
+
+    # Properties
+    links = resource.prop('links')
+    name = resource.prop('name')
+    ram = resource.prop('ram')
