95e82f97f8c9db3d4ba2fc474854a8dc61c46957
Steve Lewis <stevelle@gmail.com>
2014-09-05 16:06:40 -0700

    Add databases to instances
    Add support for list, create, and delete operations only to dbs within a dbms
    instance.
    
    Change-Id: I16eb5314fe1edfb56f9f38a74cccc39e548cd0b3

---
+++ openstack/database/v1/database.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.database import database_service
+from openstack import resource
+
+
+class Database(resource.Resource):
+    id_attribute = 'name'
+    resource_key = 'database'
+    resources_key = 'databases'
+    base_path = '/instances/%(instance_id)s/databases'
+    service = database_service.DatabaseService()
+
+    # capabilities
+    allow_create = True
+    allow_delete = True
+    allow_list = True
+
+    # Properties
+    character_set = resource.prop('character_set')
+    collate = resource.prop('collate')
+    instance_id = resource.prop('instance_id')
+    name = resource.prop('name')
