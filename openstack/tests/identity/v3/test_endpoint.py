9d85c4f4c172a5e69478422bac6eb1f08d83bb43
Jamie Lennox <jamielennox@redhat.com>
2014-09-08 12:35:06 +1000

    identity/v3 endpoint resource
    Change-Id: Ic5c27eb46d5cb5993531e4a85a19f432499bf1a8

---
+++ openstack/tests/identity/v3/test_endpoint.py
@@ -0,0 +1,49 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import testtools
+
+from openstack.identity.v3 import endpoint
+
+IDENTIFIER = 'IDENTIFIER'
+EXAMPLE = {
+    'enabled': True,
+    'id': IDENTIFIER,
+    'interface': '3',
+    'region_id': '4',
+    'service_id': '5',
+    'url': '6',
+}
+
+
+class TestEndpoint(testtools.TestCase):
+
+    def test_basic(self):
+        sot = endpoint.Endpoint()
+        self.assertEqual('endpoint', sot.resource_key)
+        self.assertEqual('endpoints', sot.resources_key)
+        self.assertEqual('/endpoints', sot.base_path)
+        self.assertEqual('identity', sot.service.service_type)
+        self.assertTrue(sot.allow_create)
+        self.assertTrue(sot.allow_retrieve)
+        self.assertTrue(sot.allow_update)
+        self.assertTrue(sot.allow_delete)
+        self.assertTrue(sot.allow_list)
+
+    def test_make_it(self):
+        sot = endpoint.Endpoint(EXAMPLE)
+        self.assertEqual(EXAMPLE['enabled'], sot.enabled)
+        self.assertEqual(EXAMPLE['id'], sot.id)
+        self.assertEqual(EXAMPLE['interface'], sot.interface)
+        self.assertEqual(EXAMPLE['region_id'], sot.region_id)
+        self.assertEqual(EXAMPLE['service_id'], sot.service_id)
+        self.assertEqual(EXAMPLE['url'], sot.url)
