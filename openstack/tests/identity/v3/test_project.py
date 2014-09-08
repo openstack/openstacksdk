3ffe147cd2c71418bef5b1977ef8a7154cfca329
Jamie Lennox <jamielennox@redhat.com>
2014-09-08 12:49:13 +1000

    identity/v3 project resource
    Change-Id: I89f590db2eee8b43b645c9aff228f2f5a0e2d5ff

---
+++ openstack/tests/identity/v3/test_project.py
@@ -0,0 +1,47 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import testtools
+
+from openstack.identity.v3 import project
+
+IDENTIFIER = 'IDENTIFIER'
+EXAMPLE = {
+    'description': '1',
+    'domain_id': '2',
+    'enabled': True,
+    'id': IDENTIFIER,
+    'name': '5',
+}
+
+
+class TestProject(testtools.TestCase):
+
+    def test_basic(self):
+        sot = project.Project()
+        self.assertEqual('project', sot.resource_key)
+        self.assertEqual('projects', sot.resources_key)
+        self.assertEqual('/projects', sot.base_path)
+        self.assertEqual('identity', sot.service.service_type)
+        self.assertTrue(sot.allow_create)
+        self.assertTrue(sot.allow_retrieve)
+        self.assertTrue(sot.allow_update)
+        self.assertTrue(sot.allow_delete)
+        self.assertTrue(sot.allow_list)
+
+    def test_make_it(self):
+        sot = project.Project(EXAMPLE)
+        self.assertEqual(EXAMPLE['description'], sot.description)
+        self.assertEqual(EXAMPLE['domain_id'], sot.domain_id)
+        self.assertEqual(EXAMPLE['enabled'], sot.enabled)
+        self.assertEqual(EXAMPLE['id'], sot.id)
+        self.assertEqual(EXAMPLE['name'], sot.name)
