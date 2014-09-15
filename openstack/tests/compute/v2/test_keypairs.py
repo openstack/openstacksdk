56a9b13ba2502765551ab5ed1ba71ae6a208716d
Terry Howe <terrylhowe@gmail.com>
2014-09-15 14:01:55 -0600

    compute/v2 keypairs resource
    Change-Id: I9ffa9dc09891fd8a8ecdbb7a479a135031f780c2

---
+++ openstack/tests/compute/v2/test_keypairs.py
@@ -0,0 +1,43 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import testtools
+
+from openstack.compute.v2 import keypairs
+
+IDENTIFIER = 'IDENTIFIER'
+EXAMPLE = {
+    'fingerprint': '1',
+    'name': '2',
+    'public_key': '3',
+}
+
+
+class TestKeypairs(testtools.TestCase):
+
+    def test_basic(self):
+        sot = keypairs.Keypairs()
+        self.assertEqual('keypairs', sot.resource_key)
+        self.assertEqual('keypairs', sot.resources_key)
+        self.assertEqual('/os-keypairs', sot.base_path)
+        self.assertEqual('compute', sot.service.service_type)
+        self.assertTrue(sot.allow_create)
+        self.assertTrue(sot.allow_retrieve)
+        self.assertTrue(sot.allow_update)
+        self.assertTrue(sot.allow_delete)
+        self.assertTrue(sot.allow_list)
+
+    def test_make_it(self):
+        sot = keypairs.Keypairs(EXAMPLE)
+        self.assertEqual(EXAMPLE['fingerprint'], sot.fingerprint)
+        self.assertEqual(EXAMPLE['name'], sot.name)
+        self.assertEqual(EXAMPLE['public_key'], sot.public_key)
