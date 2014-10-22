a696b41008c5259fa805951616e550d536a62b84
Terry Howe <terrylhowe@gmail.com>
2014-10-22 09:21:06 -0600

    Move the examples docs into the code
    I like the idea of having the examples in the code.  I don't necessarily
    like having to scroll through all those comments to get to the code.  This
    also changes the order of the documentation to summary, example, reference
    versus summary, reference, example.  I think I'm okay with that because
    :class links will anchor to the reference section.  Normally, seeing a
    simple example first can be more useful.
    
    This is a response to Doug's comment:
    https://review.openstack.org/#/c/129937/1/doc/source/user_preference.rst
    
    Change-Id: Ibbda909d2183edc6baeeec0bc56f01779334aabd

--- openstack/connection.py
+++ openstack/connection.py
@@ -16,6 +16,47 @@ The ``Connection`` class is the primary interface to the Python SDK.
 a connection to a cloud provider.  The connection has an attribute to
 access each supported service.  The service attributes are created
 dynamically based on user preferences and the service catalog.
+
+Examples
+--------
+
+At a minimum, the ``Connection`` class needs to be created with an
+authenticator or the parameters to build one.
+
+Create a connection
+~~~~~~~~~~~~~~~~~~~
+
+The following example constructor uses the identity authenticator using
+username and password.  The default settings for the transport are used
+by this connection.::
+
+    from openstack import connection
+    auth_args = {
+        'auth_url': 'http://172.20.1.108:5000/v3',
+        'project_name': 'admin',
+        'user_name': 'admin',
+        'password': 'admin',
+    }
+    conn = connection.Connection(**auth_args)
+
+List
+~~~~
+
+Services are accessed through an attribute named after the service.  A list
+of all the projects is retrieved in this manner::
+
+    projects = conn.identity.list_projects()
+
+Find or create
+~~~~~~~~~~~~~~
+If you wanted to make sure you had a network named 'jenkins', you would first
+try to find it and if that fails, you would create it::
+
+    try:
+        network = conn.network.find_network("jenkins")
+    except exceptions.ResourceNotFound:
+        network = conn.network.create_network({"name": "jenkins"})
+
 """
 
 from stevedore import driver
