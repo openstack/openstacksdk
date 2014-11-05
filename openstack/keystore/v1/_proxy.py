a8df08e6599390fdda55c2747e89dc57380ac10e
Terry Howe <terrylhowe@gmail.com>
2014-11-05 13:31:31 +0100

    keystore proxy methods and tests
    Change-Id: Id0001d8ad24e1d8ab524ee5cdcaa6b90c7afa856

--- openstack/keystore/v1/_proxy.py
+++ openstack/keystore/v1/_proxy.py
@@ -10,8 +10,66 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+from openstack.keystore.v1 import container
+from openstack.keystore.v1 import order
+from openstack.keystore.v1 import secret
+
 
 class Proxy(object):
 
     def __init__(self, session):
         self.session = session
+
+    def create_container(self, **data):
+        return container.Container(data).create(self.session)
+
+    def delete_container(self, **data):
+        container.Container(data).delete(self.session)
+
+    def find_container(self, name_or_id):
+        return container.Container.find(self.session, name_or_id)
+
+    def get_container(self, **data):
+        return container.Container(data).get(self.session)
+
+    def list_container(self):
+        return container.Container.list(self.session)
+
+    def update_container(self, **data):
+        return container.Container(data).update(self.session)
+
+    def create_order(self, **data):
+        return order.Order(data).create(self.session)
+
+    def delete_order(self, **data):
+        order.Order(data).delete(self.session)
+
+    def find_order(self, name_or_id):
+        return order.Order.find(self.session, name_or_id)
+
+    def get_order(self, **data):
+        return order.Order(data).get(self.session)
+
+    def list_order(self):
+        return order.Order.list(self.session)
+
+    def update_order(self, **data):
+        return order.Order(data).update(self.session)
+
+    def create_secret(self, **data):
+        return secret.Secret(data).create(self.session)
+
+    def delete_secret(self, **data):
+        secret.Secret(data).delete(self.session)
+
+    def find_secret(self, name_or_id):
+        return secret.Secret.find(self.session, name_or_id)
+
+    def get_secret(self, **data):
+        return secret.Secret(data).get(self.session)
+
+    def list_secret(self):
+        return secret.Secret.list(self.session)
+
+    def update_secret(self, **data):
+        return secret.Secret(data).update(self.session)
