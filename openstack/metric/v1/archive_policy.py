8958423a669507c0f3f648920659de5dfe6e30e6
Julien Danjou <julien@danjou.info>
2015-02-16 18:01:37 +0100

    metric: add archive policy support
    Change-Id: I73d2adf4d3c62469f152ee2d6693a75be354216b

---
+++ openstack/metric/v1/archive_policy.py
@@ -0,0 +1,33 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.metric import metric_service
+from openstack import resource
+
+
+class ArchivePolicy(resource.Resource):
+    base_path = '/archive_policy'
+    service = metric_service.MetricService()
+
+    # Supported Operations
+    allow_create = True
+    allow_retrieve = True
+    allow_delete = True
+    allow_list = True
+
+    id_attribute = "name"
+
+    # Properties
+    name = resource.prop('name')
+    definition = resource.prop('definition', type=list)
+    back_window = resource.prop('back_window')
+    aggregation_methods = resource.prop("aggregation_methods", type=list)
