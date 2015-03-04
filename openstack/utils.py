d0a93649ddf3d58139de76646c22b581575b9fc6
Brian Curtin <brian.curtin@rackspace.com>
2015-03-04 16:06:12 -0600

    Remove CaseInsensitiveDict
    Now that we have resource.header and it ends up being populated with
    requests.structures.CaseInsensitiveDict, we don't need to hook up our
    own implementation.
    
    Change-Id: I62202e9c5dbe4f59e0c49d590d81aee7c05eac3d

--- openstack/utils.py
+++ openstack/utils.py
@@ -10,7 +10,6 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import collections
 import logging
 import sys
 
@@ -58,60 +57,3 @@ def urljoin(*args):
     link. We generally won't care about that in client.
     """
     return '/'.join(str(a or '').strip('/') for a in args)
-
-
-class CaseInsensitiveDict(collections.MutableMapping):
-    """A case insensitive dict for handling properties
-
-    In order to have resource.prop work properly for header keys,
-    the attribute dictionary in resource.Resource has to
-    be case insensitive. This class implements a MutableMapping
-    that stores an unmodified dictionary of user-set keys and
-    values, and an internal mapping of the user-set keys to
-    their lower case equivalent.
-    """
-
-    def __init__(self, parent=None):
-        if parent is not None:
-            self._keys = dict((key.lower(), key) for key in parent)
-        else:
-            self._keys = {}
-        self._dict = parent or {}
-
-    def __getitem__(self, key):
-        if key is None:
-            raise KeyError("key is None")
-
-        return self._dict[self._keys[key.lower()]]
-
-    def __setitem__(self, key, value):
-        if key is None:
-            raise KeyError("key is None")
-
-        self._dict[key] = value
-        self._keys[key.lower()] = key
-
-    def __delitem__(self, key):
-        if key is None:
-            raise KeyError("key is None")
-
-        del self._dict[self._keys[key.lower()]]
-
-    def __contains__(self, key):
-        try:
-            key = self._keys[key.lower()]
-        except KeyError:
-            return False
-        return key in self._dict
-
-    def __iter__(self):
-        return iter(self._dict)
-
-    def __len__(self):
-        return len(self._dict)
-
-    def __repr__(self):
-        return repr(self._dict)
-
-    def copy(self):
-        return CaseInsensitiveDict(self._dict.copy())
