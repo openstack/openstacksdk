7f58e2d4614fd016ab854825f36966af219c7353
Brian Curtin <brian.curtin@rackspace.com>
2015-03-23 10:50:13 -0500

    Add new _verify to proxy base tests
    This is a temporary addition to the tests which will allow us to verify
    the new form of API calls. This is being done in lieu of adding logic
    and extra arguments to the tests, which will unnecessarily complicate
    them.
    
    This was originally a part of the common_delete_method change, but is
    needed by other later changes, so I want it out of delete so we don't
    start a huge dependency chain on the delete change.
    
    Change-Id: I27f9cfd2188acd31cee120d0347504b441fa61e9

--- openstack/tests/test_proxy_base.py
+++ openstack/tests/test_proxy_base.py
@@ -41,6 +41,34 @@ class TestProxyBase(base.TestCase):
                 self.assertEqual(expected_result, test_method())
                 mocked.assert_called_with(self.session)
 
+    # NOTE(briancurtin): This is a duplicate version of _verify that is
+    # temporarily here while we shift APIs. The difference is that
+    # calls from the Proxy classes aren't going to be going directly into
+    # the Resource layer anymore, so they don't pass in the session which
+    # was tested in assert_called_with.
+    # This is being done in lieu of adding logic and complicating
+    # the _verify method. It will be removed once there is one API to
+    # be verifying.
+    def _verify2(self, mock_method, test_method,
+                 method_args=None, method_kwargs=None,
+                 expected_args=None, expected_kwargs=None,
+                 expected_result=None):
+        with mock.patch(mock_method) as mocked:
+            mocked.return_value = expected_result
+            if any([method_args, method_kwargs,
+                    expected_args, expected_kwargs]):
+                method_args = method_args or ()
+                method_kwargs = method_kwargs or {}
+                expected_args = expected_args or ()
+                expected_kwargs = expected_kwargs or {}
+
+                self.assertEqual(expected_result, test_method(*method_args,
+                                 **method_kwargs))
+                mocked.assert_called_with(*expected_args, **expected_kwargs)
+            else:
+                self.assertEqual(expected_result, test_method())
+                mocked.assert_called_with(self.session)
+
     def verify_create(self, mock_method, test_method, **kwargs):
         self._verify(mock_method, test_method, expected_result="result",
                      **kwargs)
