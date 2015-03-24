ff25b8eb3bff5b162fc112a694f3838928f524ec
David Shrewsbury <shrewsbury.dave@gmail.com>
2015-03-24 19:07:31 +0000

    Add methods for logical router management
    Adds API methods to list existing routers, create a new logical
    router, update an existing router, and delete an existing logical
    router by name or UUID.
    
    It is considered an error if more than one router with the same
    name exists and you attempt to delete by name.
    
    Also... MOAR TESTS!!! ZOMG
    
    Change-Id: Ie6ea4eb5f2322bdda07e6db87e2cdbabea492ee9

--- shade/tests/unit/test_shade.py
+++ shade/tests/unit/test_shade.py
@@ -12,11 +12,79 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import mock
+
 import shade
 from shade.tests import base
 
 
 class TestShade(base.TestCase):
 
+    def setUp(self):
+        super(TestShade, self).setUp()
+        self.cloud = shade.openstack_cloud()
+
     def test_openstack_cloud(self):
-        self.assertIsInstance(shade.openstack_cloud(), shade.OpenStackCloud)
+        self.assertIsInstance(self.cloud, shade.OpenStackCloud)
+
+    @mock.patch.object(shade.OpenStackCloud, 'list_routers')
+    def test_get_router(self, mock_list):
+        router1 = dict(id='123', name='mickey')
+        mock_list.return_value = [router1]
+        r = self.cloud.get_router('mickey')
+        self.assertIsNotNone(r)
+        self.assertDictEqual(router1, r)
+
+    @mock.patch.object(shade.OpenStackCloud, 'list_routers')
+    def test_get_router_not_found(self, mock_list):
+        mock_list.return_value = []
+        r = self.cloud.get_router('goofy')
+        self.assertIsNone(r)
+
+    @mock.patch.object(shade.OpenStackCloud, 'neutron_client')
+    def test_create_router(self, mock_client):
+        self.cloud.create_router(name='goofy', admin_state_up=True)
+        self.assertTrue(mock_client.create_router.called)
+
+    @mock.patch.object(shade.OpenStackCloud, 'neutron_client')
+    def test_update_router(self, mock_client):
+        self.cloud.update_router(router_id=123, name='goofy')
+        self.assertTrue(mock_client.update_router.called)
+
+    @mock.patch.object(shade.OpenStackCloud, 'list_routers')
+    @mock.patch.object(shade.OpenStackCloud, 'neutron_client')
+    def test_delete_router(self, mock_client, mock_list):
+        router1 = dict(id='123', name='mickey')
+        mock_list.return_value = [router1]
+        self.cloud.delete_router('mickey')
+        self.assertTrue(mock_client.delete_router.called)
+
+    @mock.patch.object(shade.OpenStackCloud, 'list_routers')
+    @mock.patch.object(shade.OpenStackCloud, 'neutron_client')
+    def test_delete_router_not_found(self, mock_client, mock_list):
+        router1 = dict(id='123', name='mickey')
+        mock_list.return_value = [router1]
+        self.assertRaises(shade.OpenStackCloudException,
+                          self.cloud.delete_router,
+                          'goofy')
+        self.assertFalse(mock_client.delete_router.called)
+
+    @mock.patch.object(shade.OpenStackCloud, 'list_routers')
+    @mock.patch.object(shade.OpenStackCloud, 'neutron_client')
+    def test_delete_router_multiple_found(self, mock_client, mock_list):
+        router1 = dict(id='123', name='mickey')
+        router2 = dict(id='456', name='mickey')
+        mock_list.return_value = [router1, router2]
+        self.assertRaises(shade.OpenStackCloudException,
+                          self.cloud.delete_router,
+                          'mickey')
+        self.assertFalse(mock_client.delete_router.called)
+
+    @mock.patch.object(shade.OpenStackCloud, 'list_routers')
+    @mock.patch.object(shade.OpenStackCloud, 'neutron_client')
+    def test_delete_router_multiple_using_id(self, mock_client, mock_list):
+        router1 = dict(id='123', name='mickey')
+        router2 = dict(id='456', name='mickey')
+        mock_list.return_value = [router1, router2]
+        self.cloud.delete_router('123')
+        self.assertTrue(mock_client.delete_router.called)
