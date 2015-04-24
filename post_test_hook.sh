4f37c1e91c8557f2feaad93b05ad3a9ca1f83aa2
TerryHowe <terrylhowe@gmail.com>
2015-04-23 19:41:31 -0600

    Add post hook file for a functional test gate
    Add a post hook file for a functional test gate.  Right now, the
    script returns success no matter what.  I'd like to monitor how
    it goes and eventually make it live.  I figure changing the script
    to return a correct result will be easier than changing the gate.
    
    Change-Id: Ia97ce6cb86902e43c6a9942f7f866a8b7baeccbb

---
+++ post_test_hook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This is a script that kicks off a series of functional tests against a
+# OpenStack devstack cloud.  This script is intended to work as a gate
+# in project-config for the Python SDK.
+
+DIR=$(cd $(dirname "$0") && pwd)
+
+echo "Running SDK functional test suite"
+sudo -H -u stack -i <<!
+source ~stack/devstack/accrc/admin/admin
+echo 'Running tests with:'
+env | grep OS_
+cd ${DIR}
+tox -e functional
+!
+# TODO(thowe) For now we are going to ignore tests failures
+# It is easier to change this script than the gate.  The magic
+# string fro this script should probably specify the -e option
+# so the script will fail immediately if a command fails.
+exit 0
