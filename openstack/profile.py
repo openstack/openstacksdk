0894d9f9ac0464d19d5cb057035a856663064972
Everett Toews <everett.toews@rackspace.com>
2015-05-08 23:31:44 -0500

    Initial commit for the Messaging service (Zaqar)
    Start the Messaging service off with one call to create a queue.
    
    Change-Id: I7f7d8c982d0f48de3687a6697e7a8632f1f3064e

--- openstack/profile.py
+++ openstack/profile.py
@@ -59,6 +59,7 @@ from openstack import exceptions
 from openstack.identity import identity_service
 from openstack.image import image_service
 from openstack.keystore import keystore_service
+from openstack.messaging import messaging_service
 from openstack.metric import metric_service
 from openstack.network import network_service
 from openstack.object_store import object_store_service
@@ -118,6 +119,9 @@ class Profile(object):
         serv.set_visibility(None)
         self._services[serv.service_type] = serv
         serv = volume_service.VolumeService()
+        serv.set_visibility(None)
+        self._services[serv.service_type] = serv
+        serv = messaging_service.MessagingService()
         serv.set_visibility(None)
         self._services[serv.service_type] = serv
 
---
+++ openstack/tests/unit/messaging/v1/test_queue.py
@@ -0,0 +1,53 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import mock
+import testtools
+
+from openstack.messaging.v1 import queue
+
+
+FAKE_NAME = 'test_queue'
+FAKE = {
+    'name': FAKE_NAME,
+}
+
+
+class TestStack(testtools.TestCase):
+
+    def test_basic(self):
+        sot = queue.Queue()
+        self.assertEqual('queues', sot.resources_key)
+        self.assertEqual('/queues', sot.base_path)
+        self.assertEqual('messaging', sot.service.service_type)
+        self.assertTrue(sot.allow_create)
+        self.assertFalse(sot.allow_retrieve)
+        self.assertFalse(sot.allow_update)
+        self.assertFalse(sot.allow_delete)
+        self.assertFalse(sot.allow_list)
+
+    def test_make_it(self):
+        sot = queue.Queue(FAKE)
+        self.assertEqual(FAKE['name'], sot.name)
+
+    def test_create(self):
+        sess = mock.Mock()
+        sess.put = mock.Mock()
+        sess.put.return_value = mock.Mock()
+        sot = queue.Queue(FAKE)
+
+        sot.create(sess)
+
+        url = 'queues/%s' % FAKE_NAME
+        sess.put.assert_called_with(url, service=sot.service, accept=None)
+        self.assertEqual(FAKE_NAME, sot.id)
+        self.assertEqual(FAKE_NAME, sot.name)
