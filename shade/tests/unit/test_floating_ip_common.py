808a50aa4f8d004eaefb51e303f5b6311c491e86
Davide Guerri <davide.guerri@gmail.com>
2015-05-15 21:48:06 +0100

    Add Neutron/Nova Floating IP tests
    Nova/Neutron floating IP unit and functional tests.
    
    Some clouds out there are still running nova-network but we want to
    provide support for neutron specific features and concepts like ports,
    networks, subnets, etc.
    
    This change is part of a set that adds neutron support to existing
    floating IP-related functions, hiding that behind resource-oriented
    methods.
    For instance, at high level, end-users can now request that a public IP
    is assigned to an instance without worrying about the specific service,
    procedures and protocols used to provide that feature in the target
    cloud.
    
    Change-Id: Ib53923246e095283e1b470e50aeac3050b8c296e

---
+++ shade/tests/unit/test_floating_ip_common.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+test_floating_ip_common
+----------------------------------
+
+Tests floating IP resource methods for Neutron and Nova-network.
+"""
+
+from mock import patch
+from shade import meta
+from shade import OpenStackCloud
+from shade.tests.fakes import FakeServer
+from shade.tests.unit import base
+
+
+class TestFloatingIP(base.TestCase):
+    def setUp(self):
+        super(TestFloatingIP, self).setUp()
+        self.client = OpenStackCloud("cloud", {})
+
+    @patch.object(OpenStackCloud, 'get_floating_ip')
+    @patch.object(OpenStackCloud, 'attach_ip_to_server')
+    @patch.object(OpenStackCloud, 'available_floating_ip')
+    def test_add_auto_ip(
+            self, mock_available_floating_ip, mock_attach_ip_to_server,
+            mock_get_floating_ip):
+        server = FakeServer(
+            id='server-id', name='test-server', status="ACTIVE", addresses={}
+        )
+        server_dict = meta.obj_to_dict(server)
+
+        mock_available_floating_ip.return_value = {
+            "id": "this-is-a-floating-ip-id",
+            "fixed_ip_address": None,
+            "floating_ip_address": "203.0.113.29",
+            "network": "this-is-a-net-or-pool-id",
+            "attached": False,
+            "status": "ACTIVE"
+        }
+
+        self.client.add_auto_ip(server=server_dict)
+
+        mock_attach_ip_to_server.assert_called_with(
+            timeout=60, wait=False, server_id='server-id',
+            floating_ip_id='this-is-a-floating-ip-id')
+
+    @patch.object(OpenStackCloud, 'nova_client')
+    @patch.object(OpenStackCloud, 'add_ip_from_pool')
+    def test_add_ips_to_server_pool(
+            self, mock_add_ip_from_pool, mock_nova_client):
+        server = FakeServer(
+            id='server-id', name='test-server', status="ACTIVE", addresses={}
+        )
+        server_dict = meta.obj_to_dict(server)
+        pool = 'nova'
+
+        mock_nova_client.servers.get.return_value = server
+
+        self.client.add_ips_to_server(server_dict, ip_pool=pool)
+
+        mock_add_ip_from_pool.assert_called_with(server_dict, pool)
+
+    @patch.object(OpenStackCloud, 'nova_client')
+    @patch.object(OpenStackCloud, 'add_ip_list')
+    def test_add_ips_to_server_ip_list(
+            self, mock_add_ip_list, mock_nova_client):
+        server = FakeServer(
+            id='server-id', name='test-server', status="ACTIVE", addresses={}
+        )
+        server_dict = meta.obj_to_dict(server)
+        ips = ['203.0.113.29', '172.24.4.229']
+        mock_nova_client.servers.get.return_value = server
+
+        self.client.add_ips_to_server(server_dict, ips=ips)
+
+        mock_add_ip_list.assert_called_with(server_dict, ips)
+
+    @patch.object(OpenStackCloud, 'nova_client')
+    @patch.object(OpenStackCloud, 'add_auto_ip')
+    def test_add_ips_to_server_auto_ip(
+            self, mock_add_auto_ip, mock_nova_client):
+        server = FakeServer(
+            id='server-id', name='test-server', status="ACTIVE", addresses={}
+        )
+        server_dict = meta.obj_to_dict(server)
+
+        mock_nova_client.servers.get.return_value = server
+
+        self.client.add_ips_to_server(server_dict)
+
+        mock_add_auto_ip.assert_called_with(server_dict)
