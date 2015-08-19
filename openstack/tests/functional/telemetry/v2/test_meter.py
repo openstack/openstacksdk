36e5af74e6ecaba4cea4bfcd2c1ef997d1e15eb0
TerryHowe <terrylhowe@gmail.com>
2015-08-19 13:11:14 -0600

    Make sure there is data for the meter test
    The meter tests relies on someone else creating a meter.  If
    this test happens to run on a brand new system and it runs before
    the object store tests, it may fail.  For some object store
    activity before the test.
    
    Change-Id: Ie9b7775bad4550842bfdaebd893eb8293590b7ff

--- openstack/tests/functional/telemetry/v2/test_meter.py
+++ openstack/tests/functional/telemetry/v2/test_meter.py
@@ -10,11 +10,20 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import uuid
+
 from openstack.tests.functional import base
 
 
 class TestMeter(base.BaseFunctionalTest):
 
     def test_list(self):
+        # TODO(thowe): Remove this in favor of create_meter call.
+        # Since we do not have a create meter method at the moment
+        # make sure there is some data in there
+        name = uuid.uuid4().hex
+        tainer = self.conn.object_store.create_container(name=name)
+        self.conn.object_store.delete_container(tainer)
+
         names = set([o.name for o in self.conn.telemetry.meters()])
         self.assertIn('storage.objects', names)
