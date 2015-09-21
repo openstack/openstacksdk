84904f3f38fbc276e19e94e3ad617a5b27166d20
James E. Blair <jeblair@linux.vnet.ibm.com>
2015-09-21 16:06:00 -0700

    Make inner_exception a private member
    So we don't mislead anyone into thinking it's part of the public
    API.
    
    Change-Id: Ibc74eeec733bc56ba4c05c27f7e3de3c89b5f31d

--- shade/exc.py
+++ shade/exc.py
@@ -26,19 +26,19 @@ class OpenStackCloudException(Exception):
             args.append(extra_data)
         super(OpenStackCloudException, self).__init__(*args)
         self.extra_data = extra_data
-        self.inner_exception = sys.exc_info()
-        if self.inner_exception and self.inner_exception[1]:
-            log.error(message, exc_info=self.inner_exception)
+        self._inner_exception = sys.exc_info()
+        if self._inner_exception and self._inner_exception[1]:
+            log.error(message, exc_info=self._inner_exception)
 
     def __str__(self):
         message = Exception.__str__(self)
         if self.extra_data is not None:
             message = "%s (Extra: %s)" % (message, self.extra_data)
-        if (self.inner_exception and self.inner_exception[1]
-                and hasattr(self.inner_exception[1], 'message')):
+        if (self._inner_exception and self._inner_exception[1]
+                and hasattr(self._inner_exception[1], 'message')):
             message = "%s (Inner Exception: %s)" % (
                 message,
-                self.inner_exception[1].message)
+                self._inner_exception[1].message)
         return message
 
 
