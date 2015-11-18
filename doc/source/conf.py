52e525fcd576e93dbd4ac511463ab1d0a0275cf3
Anne Gentle <anne@openstack.org>
2015-11-18 11:30:53 -0600

    Updates doc enviro to use OpenStack Docs theme
    For building to developer.openstack.org, want to match the
    app dev docs.
    Partial-bug: 1479977
    
    Change-Id: I684e9d4fd3128f022e511ab2b8c75245cfaf19bf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,6 +15,8 @@
 import os
 import sys
 
+import openstackdocstheme
+
 sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ----------------------------------------------------
 
@@ -30,6 +32,9 @@ extensions = [
 # text edit cycles.
 # execute "export SPHINX_DEBUG=1" in your terminal to disable
 
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
 # The suffix of source filenames.
 source_suffix = '.rst'
 
@@ -38,7 +43,31 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'python-openstacksdk'
-copyright = u'2013, OpenStack Foundation'
+copyright = u'2015, OpenStack Foundation'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# "version" and "release" are used by the "log-a-bug" feature
+#
+# The short X.Y version.
+version = '1.0'
+# The full version, including alpha/beta/rc tags.
+release = '1.0'
+
+# We ask git for the SHA checksum
+# The git SHA checksum is used by "log-a-bug"
+giturl = u'http://git.openstack.org/cgit/openstack/python-openstacksdk/tree/doc/source'
+git_cmd = "/usr/bin/git log | head -n1 | cut -f2 -d' '"
+gitsha = os.popen(git_cmd).read().strip('\n')
+# tag that reported bugs will be tagged with when using the "log a bug"
+# clickthrough on each page, such as user-guide or install-guide
+bug_tag = "docs"
+# source tree
+pwd = os.getcwd()
+# html_context allows us to pass arbitrary values into the html template
+html_context = { "pwd":pwd, "gitsha":gitsha, "bug_tag": bug_tag}
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
@@ -55,13 +84,14 @@ autodoc_member_order = "bysource"
 # Locations to exclude when looking for source files.
 exclude_patterns = []
 
-# -- Options for HTML output --------------------------------------------------
+# -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  Major themes that come with
-# Sphinx are currently 'default' and 'sphinxdoc'.
-# html_theme_path = ["."]
-# html_theme = '_theme'
-# html_static_path = ['static']
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'openstackdocs'
+
+# Add any paths that contain custom themes here, relative to this directory.
+html_theme_path = [openstackdocstheme.get_html_theme_path()]
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
