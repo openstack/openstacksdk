eeedb20a60cb0f9e9177ec958cd977794f2566d4
Andreas Jaeger <aj@suse.com>
2015-12-11 17:09:25 +0100

    Fix "report a bug" launchpad project
    For docs set the launchpad project users report bugs
    against to "python-openstacksdk". Users can report bugs
    using the "bug icon" that will
    directly link to the launchpad project, it currently goes to
    "openstack-manuals" which is wrong for this content.
    
    This variable is used by openstackdocstheme 1.2.6.
    
    Also, update comments for the variables passed to openstackdocstheme.
    
    Change-Id: Id036c6373092673ce0c54be1b64ea87d1de89b88
    Related-Bug: #1524476

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -55,18 +55,24 @@ version = '1.0'
 # The full version, including alpha/beta/rc tags.
 release = '1.0'
 
-# We ask git for the SHA checksum
-# The git SHA checksum is used by "log-a-bug"
+# A few variables have to be set for the log-a-bug feature.
+#   giturl: The location of conf.py on Git. Must be set manually.
+#   gitsha: The SHA checksum of the bug description. Extracted from git log.
+#   bug_tag: Tag for categorizing the bug. Must be set manually.
+#   bug_project: Launchpad project to file bugs against.
+# These variables are passed to the logabug code via html_context.
 giturl = u'http://git.openstack.org/cgit/openstack/python-openstacksdk/tree/doc/source'
 git_cmd = "/usr/bin/git log | head -n1 | cut -f2 -d' '"
 gitsha = os.popen(git_cmd).read().strip('\n')
-# tag that reported bugs will be tagged with when using the "log a bug"
-# clickthrough on each page, such as user-guide or install-guide
 bug_tag = "docs"
 # source tree
 pwd = os.getcwd()
 # html_context allows us to pass arbitrary values into the html template
-html_context = { "pwd":pwd, "gitsha":gitsha, "bug_tag": bug_tag}
+html_context = {"pwd": pwd,
+                "gitsha": gitsha,
+                "bug_tag": bug_tag,
+                "giturl": giturl,
+                "bug_project": "python-openstacksdk"}
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
