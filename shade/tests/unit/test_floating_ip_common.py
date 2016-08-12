53b1fc4cdee8e460361c17f70571e883059323c1
Monty Taylor <mordred@inaugust.com>
2016-08-12 14:34:43 -0500

    Base auto_ip on interface_ip not public_v4
    Currently the auto_ip logic checks for a public v4 address to determine
    if it should make a FIP. However, we have richer logic that provides
    interface_ip that takes in to account IPv6 as well as configured choices
    as to what the "default" network should be. That is what should really
    be cared about - does the server have an ip that would be considered the
    thing to connect to it on.
    
    Change-Id: Ica1feb31cc3477c6336a749c88f4706efd20484f

--- shade/tests/unit/test_floating_ip_common.py
+++ shade/tests/unit/test_floating_ip_common.py
@@ -80,6 +80,118 @@ class TestFloatingIP(base.TestCase):
             server_dict, pool, reuse=True, wait=False, timeout=60,
             fixed_address=None, nat_destination=None)
 
+    @patch.object(OpenStackCloud, 'has_service')
+    @patch.object(OpenStackCloud, 'get_floating_ip')
+    @patch.object(OpenStackCloud, '_add_auto_ip')
+    def test_add_ips_to_server_ipv6_only(
+            self, mock_add_auto_ip,
+            mock_get_floating_ip,
+            mock_has_service):
+        self.cloud._floating_ip_source = None
+        self.cloud.force_ipv4 = False
+        self.cloud._local_ipv6 = True
+        mock_has_service.return_value = False
+        server = FakeServer(
+            id='server-id', name='test-server', status="ACTIVE",
+            addresses={
+                'private': [{
+                    'addr': "10.223.160.141",
+                    'version': 4
+                }],
+                'public': [{
+                    u'OS-EXT-IPS-MAC:mac_addr': u'fa:16:3e:ae:7d:42',
+                    u'OS-EXT-IPS:type': u'fixed',
+                    'addr': "2001:4800:7819:103:be76:4eff:fe05:8525",
+                    'version': 6
+                }]
+            }
+        )
+        server_dict = meta.add_server_interfaces(
+            self.cloud, meta.obj_to_dict(server))
+
+        new_server = self.client.add_ips_to_server(server=server_dict)
+        mock_get_floating_ip.assert_not_called()
+        mock_add_auto_ip.assert_not_called()
+        self.assertEqual(
+            new_server['interface_ip'],
+            '2001:4800:7819:103:be76:4eff:fe05:8525')
+        self.assertEqual(new_server['private_v4'], '10.223.160.141')
+        self.assertEqual(new_server['public_v4'], '')
+        self.assertEqual(
+            new_server['public_v6'], '2001:4800:7819:103:be76:4eff:fe05:8525')
+
+    @patch.object(OpenStackCloud, 'has_service')
+    @patch.object(OpenStackCloud, 'get_floating_ip')
+    @patch.object(OpenStackCloud, '_add_auto_ip')
+    def test_add_ips_to_server_rackspace(
+            self, mock_add_auto_ip,
+            mock_get_floating_ip,
+            mock_has_service):
+        self.cloud._floating_ip_source = None
+        self.cloud.force_ipv4 = False
+        self.cloud._local_ipv6 = True
+        mock_has_service.return_value = False
+        server = FakeServer(
+            id='server-id', name='test-server', status="ACTIVE",
+            addresses={
+                'private': [{
+                    'addr': "10.223.160.141",
+                    'version': 4
+                }],
+                'public': [{
+                    'addr': "104.130.246.91",
+                    'version': 4
+                }, {
+                    'addr': "2001:4800:7819:103:be76:4eff:fe05:8525",
+                    'version': 6
+                }]
+            }
+        )
+        server_dict = meta.add_server_interfaces(
+            self.cloud, meta.obj_to_dict(server))
+
+        new_server = self.client.add_ips_to_server(server=server_dict)
+        mock_get_floating_ip.assert_not_called()
+        mock_add_auto_ip.assert_not_called()
+        self.assertEqual(
+            new_server['interface_ip'],
+            '2001:4800:7819:103:be76:4eff:fe05:8525')
+
+    @patch.object(OpenStackCloud, 'has_service')
+    @patch.object(OpenStackCloud, 'get_floating_ip')
+    @patch.object(OpenStackCloud, '_add_auto_ip')
+    def test_add_ips_to_server_rackspace_local_ipv4(
+            self, mock_add_auto_ip,
+            mock_get_floating_ip,
+            mock_has_service):
+        self.cloud._floating_ip_source = None
+        self.cloud.force_ipv4 = False
+        self.cloud._local_ipv6 = False
+        mock_has_service.return_value = False
+        server = FakeServer(
+            id='server-id', name='test-server', status="ACTIVE",
+            addresses={
+                'private': [{
+                    'addr': "10.223.160.141",
+                    'version': 4
+                }],
+                'public': [{
+                    'addr': "104.130.246.91",
+                    'version': 4
+                }, {
+                    'addr': "2001:4800:7819:103:be76:4eff:fe05:8525",
+                    'version': 6
+                }]
+            }
+        )
+        server_dict = meta.add_server_interfaces(
+            self.cloud, meta.obj_to_dict(server))
+
+        new_server = self.client.add_ips_to_server(server=server_dict)
+        mock_get_floating_ip.assert_not_called()
+        mock_add_auto_ip.assert_not_called()
+        self.assertEqual(new_server['interface_ip'], '104.130.246.91')
+
     @patch.object(OpenStackCloud, 'nova_client')
     @patch.object(OpenStackCloud, 'add_ip_list')
     def test_add_ips_to_server_ip_list(
