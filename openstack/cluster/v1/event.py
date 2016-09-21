ae57aceb92b1b8fea56cddc5e86d670d9547667a
Qiming Teng <tengqim@cn.ibm.com>
2016-09-21 12:20:53 +0000

    Revert "Event list can not display "timestamp"
    This patch is against sdk's convention for naming datetime fields.
    
    This reverts commit 619032ea1876078f22a1d8dec9509db4a9769f63.
    
    Change-Id: I5f6be37265a9cf139bff740f68423ecf3d5d9f47

--- openstack/cluster/v1/event.py
+++ openstack/cluster/v1/event.py
@@ -31,7 +31,7 @@ class Event(resource.Resource):
 
     # Properties
     #: Timestamp string (in ISO8601 format) when the event was generated.
-    timestamp = resource.Body('timestamp')
+    generated_at = resource.Body('timestamp')
     #: The UUID of the object related to this event.
     obj_id = resource.Body('oid')
     #: The name of the object related to this event.
