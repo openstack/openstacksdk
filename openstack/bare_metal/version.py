65ca1de857174d317d1ec54225eddd970fa18959
Tang Chen <chen.tang@easystack.cn>
2016-10-14 19:05:09 +0800

    Prepare for baremetal API implementation
    This patch adds some basic files and classes for
    baremetal API implementation.
    
    Change-Id: I19a34f8430c64f1e2e8f62045f6e744d251fb595
    Implements: blueprint implement-baremental-api

---
+++ openstack/bare_metal/version.py
@@ -0,0 +1,31 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.bare_metal import bare_metal_service
+from openstack import resource2
+
+
+class Version(resource2.Resource):
+    resource_key = 'version'
+    resources_key = 'versions'
+    base_path = '/'
+    service = bare_metal_service.BareMetalService(
+        version=bare_metal_service.BareMetalService.UNVERSIONED
+    )
+
+    # Capabilities
+    allow_list = True
+
+    # Attributes
+    links = resource2.Body('links')
+    status = resource2.Body('status')
+    updated = resource2.Body('updated')
