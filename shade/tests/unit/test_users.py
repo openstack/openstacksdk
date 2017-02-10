4161e5c79aead7861ed8c4737164bad914a0fdb5
Morgan Fainberg <morgan.fainberg@gmail.com>
2017-02-10 13:51:50 -0800

    Convert test_users to requests_mock
    Convert the remaining tests for test_users to use requests mock.
    
    Change-Id: I504cc3c0be883c8e5c6323cd535a04834eb97119

--- shade/tests/unit/test_users.py
+++ shade/tests/unit/test_users.py
@@ -11,15 +11,11 @@
 # under the License.
 
 import collections
-import mock
 import uuid
 
-import munch
-import os_client_config as occ
 import testtools
 
 import shade
-from shade.tests import fakes
 from shade.tests.unit import base
 
 
@@ -29,19 +25,36 @@ _UserData = collections.namedtuple(
     'json_response, json_request')
 
 
+_GroupData = collections.namedtuple(
+    'GroupData',
+    'group_id, group_name, domain_id, description, json_response')
+
+
 class TestUsers(base.RequestsMockTestCase):
 
-    def _get_mock_url(self, v3=False, append=None, interface='admin'):
+    def _get_mock_url(self, v3=False, append=None, resource='users',
+                      interface='admin'):
         service_catalog = self.cloud.keystone_session.auth.get_access(
             self.cloud.keystone_session).service_catalog
         endpoint_url = service_catalog.url_for(
             service_type='identity',
             interface=interface)
-        to_join = [endpoint_url, 'users']
+        to_join = [endpoint_url, resource]
         if v3:
             to_join.insert(1, 'v3')
         to_join.extend(append or [])
         return '/'.join(to_join)
+
+    def _get_group_data(self, name=None, domain_id=None, description=None):
+        group_id = uuid.uuid4().hex
+        name or self.getUniqueString('groupname')
+        domain_id = uuid.UUID(domain_id or uuid.uuid4().hex).hex
+        response = {'id': group_id, 'name': name, 'domain_id': domain_id}
+        if description is not None:
+            response['description'] = description
+
+        return _GroupData(group_id, name, domain_id, description,
+                          {'group': response})
 
     def _get_user_data(self, name=None, password=None, **kwargs):
 
@@ -102,7 +115,8 @@ class TestUsers(base.RequestsMockTestCase):
         self.register_uri(
             'POST',
             self._get_mock_url(v3=True), status_code=204,
-            json=user_data.json_response)
+            json=user_data.json_response,
+            validate=user_data.json_request)
         self.register_uri(
             'GET',
             self._get_mock_url(v3=True, append=[user_data.user_id]),
@@ -119,96 +133,141 @@ class TestUsers(base.RequestsMockTestCase):
         self.assertEqual(user_data.user_id, user.id)
         self.assert_calls()
 
-    @mock.patch.object(occ.cloud_config.CloudConfig, 'get_api_version')
-    @mock.patch.object(shade.OpenStackCloud, 'keystone_client')
-    def test_update_user_password_v2(self, mock_keystone, mock_api_version):
-        mock_api_version.return_value = '2'
-        name = 'Mickey Mouse'
-        email = 'mickey@disney.com'
-        password = 'mice-rule'
-        domain_id = '1'
-        user = {'id': '1', 'name': name, 'email': email, 'description': None}
-        fake_user = fakes.FakeUser(**user)
-        munch_fake_user = munch.Munch(user)
-        mock_keystone.users.list.return_value = [fake_user]
-        mock_keystone.users.get.return_value = fake_user
-        mock_keystone.users.update.return_value = fake_user
-        mock_keystone.users.update_password.return_value = fake_user
+    def test_update_user_password_v2(self):
+        self.use_keystone_v2()
+        user_data = self._get_user_data(email='test@example.com')
+        self.register_uri('GET', self._get_mock_url(), status_code=200,
+                          json={'users': [user_data.json_response['user']]})
+        self.register_uri('GET',
+                          self._get_mock_url(append=[user_data.user_id]),
+                          json=user_data.json_response)
+        self.register_uri(
+            'PUT',
+            self._get_mock_url(
+                append=[user_data.user_id, 'OS-KSADM', 'password']),
+            status_code=204, json=user_data.json_response,
+            validate=dict(json={'user': {'password': user_data.password}}))
+        self.register_uri('GET',
+                          self._get_mock_url(append=[user_data.user_id]),
+                          json=user_data.json_response)
+        # NOTE(notmorgan): when keystoneclient is dropped, the extra call is
+        # not needed as it is a blank put. Keystoneclient has very limited
+        # logic and does odd things when updates inclue passwords in v2
+        # keystone.
+        self.register_uri(
+            'PUT',
+            self._get_mock_url(append=[user_data.user_id]),
+            status_code=204, json=user_data.json_response,
+            validate=dict(json={'user': {}}))
+        self.register_uri('GET',
+                          self._get_mock_url(append=[user_data.user_id]),
+                          json=user_data.json_response)
         user = self.op_cloud.update_user(
-            name, name=name, email=email,
-            password=password,
-            domain_id=domain_id)
-        mock_keystone.users.update.assert_called_once_with(
-            user=munch_fake_user, name=name, email=email)
-        mock_keystone.users.update_password.assert_called_once_with(
-            user=munch_fake_user, password=password)
-        self.assertEqual(name, user.name)
-        self.assertEqual(email, user.email)
+            user_data.user_id, password=user_data.password)
+        self.assertEqual(user_data.name, user.name)
+        self.assertEqual(user_data.email, user.email)
+        self.assert_calls()
 
-    @mock.patch.object(occ.cloud_config.CloudConfig, 'get_api_version')
-    @mock.patch.object(shade.OpenStackCloud, 'keystone_client')
-    def test_create_user_v3_no_domain(self, mock_keystone, mock_api_version):
-        mock_api_version.return_value = '3'
-        name = 'Mickey Mouse'
-        email = 'mickey@disney.com'
-        password = 'mice-rule'
+    def test_create_user_v3_no_domain(self):
+        user_data = self._get_user_data(domain_id=uuid.uuid4().hex,
+                                        email='test@example.com')
         with testtools.ExpectedException(
                 shade.OpenStackCloudException,
                 "User or project creation requires an explicit"
                 " domain_id argument."
         ):
             self.op_cloud.create_user(
-                name=name, email=email, password=password)
+                name=user_data.name, email=user_data.email,
+                password=user_data.password)
 
-    @mock.patch.object(shade.OpenStackCloud, 'get_user_by_id')
-    @mock.patch.object(shade.OpenStackCloud, 'get_user')
-    @mock.patch.object(shade.OpenStackCloud, 'keystone_client')
-    def test_delete_user(self, mock_keystone, mock_get_user, mock_get_by_id):
-        mock_get_user.return_value = dict(id='123')
-        fake_user = fakes.FakeUser('123', 'email', 'name')
-        mock_get_by_id.return_value = fake_user
-        self.assertTrue(self.op_cloud.delete_user('name'))
-        mock_get_by_id.assert_called_once_with('123', normalize=False)
-        mock_keystone.users.delete.assert_called_once_with(user=fake_user)
+    def test_delete_user(self):
+        self._add_discovery_uri_call()
+        user_data = self._get_user_data(domain_id=uuid.uuid4().hex)
+        self.register_uri('GET', self._get_mock_url(v3=True), status_code=200,
+                          json={'users': [user_data.json_response['user']]})
+        self.register_uri('GET',
+                          self._get_mock_url(v3=True,
+                                             append=[user_data.user_id]),
+                          status_code=200, json=user_data.json_response)
+        self.register_uri('DELETE',
+                          self._get_mock_url(v3=True,
+                                             append=[user_data.user_id]),
+                          status_code=204)
+        self.op_cloud.delete_user(user_data.name)
+        self.assert_calls()
 
-    @mock.patch.object(shade.OpenStackCloud, 'get_user')
-    @mock.patch.object(shade.OpenStackCloud, 'keystone_client')
-    def test_delete_user_not_found(self, mock_keystone, mock_get_user):
-        mock_get_user.return_value = None
-        self.assertFalse(self.op_cloud.delete_user('name'))
-        self.assertFalse(mock_keystone.users.delete.called)
+    def test_delete_user_not_found(self):
+        self._add_discovery_uri_call()
+        self.register_uri('GET',
+                          self._get_mock_url(v3=True),
+                          status_code=200,
+                          json={'users': []})
+        self.assertFalse(self.op_cloud.delete_user(self.getUniqueString()))
 
-    @mock.patch.object(shade.OpenStackCloud, 'get_user')
-    @mock.patch.object(shade.OperatorCloud, 'get_group')
-    @mock.patch.object(shade.OpenStackCloud, 'keystone_client')
-    def test_add_user_to_group(self, mock_keystone, mock_group, mock_user):
-        mock_user.return_value = munch.Munch(dict(id=1))
-        mock_group.return_value = munch.Munch(dict(id=2))
-        self.op_cloud.add_user_to_group("user", "group")
-        mock_keystone.users.add_to_group.assert_called_once_with(
-            user=1, group=2
-        )
+    def test_add_user_to_group(self):
+        self._add_discovery_uri_call()
+        user_data = self._get_user_data()
+        group_data = self._get_group_data()
+        self.register_uri('GET',
+                          self._get_mock_url(v3=True),
+                          status_code=200,
+                          json={'users': [user_data.json_response['user']]})
+        self.register_uri(
+            'GET',
+            self._get_mock_url(v3=True, resource='groups'),
+            status_code=200,
+            json={'groups': [group_data.json_response['group']]})
+        self.register_uri(
+            'PUT',
+            self._get_mock_url(
+                v3=True, resource='groups',
+                append=[group_data.group_id, 'users', user_data.user_id]),
+            status_code=200)
+        self.op_cloud.add_user_to_group(user_data.user_id, group_data.group_id)
+        self.assert_calls()
 
-    @mock.patch.object(shade.OpenStackCloud, 'get_user')
-    @mock.patch.object(shade.OperatorCloud, 'get_group')
-    @mock.patch.object(shade.OpenStackCloud, 'keystone_client')
-    def test_is_user_in_group(self, mock_keystone, mock_group, mock_user):
-        mock_user.return_value = munch.Munch(dict(id=1))
-        mock_group.return_value = munch.Munch(dict(id=2))
-        mock_keystone.users.check_in_group.return_value = True
-        self.assertTrue(self.op_cloud.is_user_in_group("user", "group"))
-        mock_keystone.users.check_in_group.assert_called_once_with(
-            user=1, group=2
-        )
+    def test_is_user_in_group(self):
+        self._add_discovery_uri_call()
+        user_data = self._get_user_data()
+        group_data = self._get_group_data()
+        self.register_uri('GET',
+                          self._get_mock_url(v3=True),
+                          status_code=200,
+                          json={'users': [user_data.json_response['user']]})
+        self.register_uri(
+            'GET',
+            self._get_mock_url(v3=True, resource='groups'),
+            status_code=200,
+            json={'groups': [group_data.json_response['group']]})
+        self.register_uri(
+            'HEAD',
+            self._get_mock_url(
+                v3=True, resource='groups',
+                append=[group_data.group_id, 'users', user_data.user_id]),
+            status_code=204)
+        self.assertTrue(self.op_cloud.is_user_in_group(
+            user_data.user_id, group_data.group_id))
+        self.assert_calls()
 
-    @mock.patch.object(shade.OpenStackCloud, 'get_user')
-    @mock.patch.object(shade.OperatorCloud, 'get_group')
-    @mock.patch.object(shade.OpenStackCloud, 'keystone_client')
-    def test_remove_user_from_group(self, mock_keystone, mock_group,
-                                    mock_user):
-        mock_user.return_value = munch.Munch(dict(id=1))
-        mock_group.return_value = munch.Munch(dict(id=2))
-        self.op_cloud.remove_user_from_group("user", "group")
-        mock_keystone.users.remove_from_group.assert_called_once_with(
-            user=1, group=2
-        )
+    def test_remove_user_from_group(self):
+        self._add_discovery_uri_call()
+        user_data = self._get_user_data()
+        group_data = self._get_group_data()
+        self.register_uri('GET',
+                          self._get_mock_url(v3=True),
+                          status_code=200,
+                          json={'users': [user_data.json_response['user']]})
+        self.register_uri(
+            'GET',
+            self._get_mock_url(v3=True, resource='groups'),
+            status_code=200,
+            json={'groups': [group_data.json_response['group']]})
+        self.register_uri(
+            'DELETE',
+            self._get_mock_url(
+                v3=True, resource='groups',
+                append=[group_data.group_id, 'users', user_data.user_id]),
+            status_code=204)
+        self.op_cloud.remove_user_from_group(user_data.user_id,
+                                             group_data.group_id)
+        self.assert_calls()
