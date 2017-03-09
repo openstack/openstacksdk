189353e4eb110facbabf9882e0af1ef16ced600f
Terry Howe <terrylhowe@gmail.com>
2017-03-09 11:36:51 -0700

    Fix network quota test so it works on gate
    The gate does not create quotas by default, but devstack does.
    This test is not important enough to make work for the gate which
    would probably require some reconfiguration, but it is nice to
    have it for devstack.
    
    Change-Id: I6618b5ee8c1dde7773b83e8ba97092f30d595e8a
    Partial-bug: #1665495

--- openstack/tests/functional/network/v2/test_quota.py
+++ openstack/tests/functional/network/v2/test_quota.py
@@ -10,34 +10,19 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import uuid
-
 from openstack.tests.functional import base
 
 
 class TestQuota(base.BaseFunctionalTest):
 
-    PROJECT_NAME = 'project-' + uuid.uuid4().hex
-    PROJECT = None
-
-    @classmethod
-    def setUpClass(cls):
-        super(TestQuota, cls).setUpClass()
-        # Need a project to have a quota
-        cls.PROJECT = cls.conn.identity.create_project(name=cls.PROJECT_NAME)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.conn.identity.delete_project(cls.PROJECT.id)
-
     def test_list(self):
-        qot = self.conn.network.quotas().next()
-        self.assertIsNotNone(qot.project_id)
-        self.assertIsNotNone(qot.networks)
+        for qot in self.conn.network.quotas():
+            self.assertIsNotNone(qot.project_id)
+            self.assertIsNotNone(qot.networks)
 
     def test_set(self):
         attrs = {'networks': 123456789}
-        project_quota = self.conn.network.quotas().next()
-        self.conn.network.update_quota(project_quota, **attrs)
-        new_quota = self.conn.network.get_quota(project_quota.project_id)
-        self.assertEqual(123456789, new_quota.networks)
+        for project_quota in self.conn.network.quotas():
+            self.conn.network.update_quota(project_quota, **attrs)
+            new_quota = self.conn.network.get_quota(project_quota.project_id)
+            self.assertEqual(123456789, new_quota.networks)
