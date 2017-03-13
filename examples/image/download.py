237041aff9d99ac840572742467772edf1f4d5ef
Brian Curtin <brian@python.org>
2017-03-13 11:14:45 -0400

    Add image download example
    I forgot to `git add` this file to the
    Ie62ebcc895ca893321a10def18ac5d74c7c843b9 change and recently noticed it
    causing `tox -e pep8` to fail locally.
    
    Change-Id: Ib80eda80f27457f2db2672d58c4a6c4a7ecd3d7c

---
+++ examples/image/download.py
@@ -0,0 +1,64 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import hashlib
+
+"""
+Download an image with the Image service.
+
+For a full guide see
+http://developer.openstack.org/sdks/python/openstacksdk/users/guides/image.html
+"""
+
+
+def download_image_stream(conn):
+    print("Download Image via streaming:")
+
+    # Find the image you would like to download.
+    image = conn.image.find_image("myimage")
+
+    # As the actual download now takes place outside of the library
+    # and in your own code, you are now responsible for checking
+    # the integrity of the data. Create an MD5 has to be computed
+    # after all of the data has been consumed.
+    md5 = hashlib.md5()
+
+    with open("myimage.qcow2", "wb") as local_image:
+        response = conn.image.download_image(image, stream=True)
+
+        # Read only 1024 bytes of memory at a time until
+        # all of the image data has been consumed.
+        for chunk in response.iter_content(chunk_size=1024):
+
+            # With each chunk, add it to the hash to be computed.
+            md5.update(chunk)
+
+            local_image.write(chunk)
+
+        # Now that you've consumed all of the data the response gave you,
+        # ensure that the checksums of what the server offered and
+        # what you downloaded are the same.
+        if response.headers["Content-MD5"] != md5.hexdigest():
+            raise Exception("Checksum mismatch in downloaded content")
+
+
+def download_image(conn):
+    print("Download Image:")
+
+    # Find the image you would like to download.
+    image = conn.image.find_image("myimage")
+
+    with open("myimage.qcow2", "w") as local_image:
+        response = conn.image.download_image(image)
+
+        # Response will contain the entire contents of the Image.
+        local_image.write(response)
