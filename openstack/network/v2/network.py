8dc68b89ff00d9f490e1c63882386d18974158eb
Akihiro Motoki <amotoki@gmail.com>
2017-03-27 10:14:43 +0000

    Filtering support by is_router_external to network resource
    Filtering networks by is_router_external (router:external) is missing
    in the current SDK. This is important from the perspective of migration
    from neutron CLI to OSC because networks with router:external True are
    regarded as floating IP pools.
    
    Change-Id: I164bee3c9de00bdd0513114577e94b68f6cc7e5a
    Closes-Bug: #1676355

--- openstack/network/v2/network.py
+++ openstack/network/v2/network.py
@@ -34,6 +34,7 @@ class Network(resource.Resource):
         ipv6_address_scope_id='ipv6_address_scope',
         is_admin_state_up='admin_state_up',
         is_port_security_enabled='port_security_enabled',
+        is_router_external='router:external',
         is_shared='shared',
         provider_network_type='provider:network_type',
         provider_physical_network='provider:physical_network',
--- openstack/tests/unit/network/v2/test_network.py
+++ openstack/tests/unit/network/v2/test_network.py
@@ -93,6 +93,25 @@ class TestNetwork(testtools.TestCase):
         self.assertEqual(EXAMPLE['subnets'], sot.subnet_ids)
         self.assertEqual(EXAMPLE['updated_at'], sot.updated_at)
 
+        self.assertDictEqual(
+            {'limit': 'limit',
+             'marker': 'marker',
+             'description': 'description',
+             'name': 'name',
+             'project_id': 'project_id',
+             'status': 'status',
+             'ipv4_address_scope_id': 'ipv4_address_scope',
+             'ipv6_address_scope_id': 'ipv6_address_scope',
+             'is_admin_state_up': 'admin_state_up',
+             'is_port_security_enabled': 'port_security_enabled',
+             'is_router_external': 'router:external',
+             'is_shared': 'shared',
+             'provider_network_type': 'provider:network_type',
+             'provider_physical_network': 'provider:physical_network',
+             'provider_segmentation_id': 'provider:segmentation_id'
+             },
+            sot._query_mapping._mapping)
+
 
 class TestDHCPAgentHostingNetwork(testtools.TestCase):
 
