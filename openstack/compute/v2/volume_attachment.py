885ae5f2b7f80d8653beb8f02b39fcba9511ea53
Yuval Shalev <shalev67@gmail.com>
2017-04-10 17:08:27 +0300

    Add support for volume attachments in compute v2
    This change adds a VolumeAttachment resource and the
    accompanying proxy methods for Compute v2.
    
    Change-Id: Ib9a10599cdc83426163668bbcc6f845218fa1a59

---
+++ openstack/compute/v2/volume_attachment.py
@@ -0,0 +1,41 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.compute import compute_service
+from openstack import resource2
+
+
+class VolumeAttachment(resource2.Resource):
+    resource_key = 'volumeAttachment'
+    resources_key = 'volumeAttachments'
+    base_path = '/servers/%(server_id)s/os-volume_attachments'
+    service = compute_service.ComputeService()
+
+    # capabilities
+    allow_create = True
+    allow_get = True
+    allow_update = False
+    allow_delete = True
+    allow_list = True
+
+    _query_mapping = resource2.QueryParameters("limit", "offset")
+
+    #: Name of the device such as, /dev/vdb.
+    device = resource2.Body('device')
+    #: The ID of the attachment.
+    id = resource2.Body('id')
+    #: The ID for the server.
+    server_id = resource2.URI('server_id')
+    #: The ID of the attached volume.
+    volume_id = resource2.Body('volumeId')
+    #: The ID of the attachment you want to delete or update.
+    attachment_id = resource2.Body('attachment_id', alternate_id=True)
