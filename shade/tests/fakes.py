3aec23cfd2a0477693e6684a97bfb3abbd3cd937
Monty Taylor <mordred@inaugust.com>
2017-06-18 10:34:34 -0500

    Convert server group tests to requests_mock
    Change-Id: Ic4b137073e0c662d26cac254611708b3f31734e0

--- shade/tests/fakes.py
+++ shade/tests/fakes.py
@@ -258,11 +258,14 @@ class FakeFloatingIP(object):
         self.instance_id = instance_id
 
 
-class FakeServerGroup(object):
-    def __init__(self, id, name, policies):
-        self.id = id
-        self.name = name
-        self.policies = policies
+def make_fake_server_group(id, name, policies):
+    return json.loads(json.dumps({
+        'id': id,
+        'name': name,
+        'policies': policies,
+        'members': [],
+        'metadata': {},
+    }))
 
 
 class FakeVolume(object):
