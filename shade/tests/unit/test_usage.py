56244f54103c076444a1e8793fa78b6753350961
Monty Taylor <mordred@inaugust.com>
2017-06-18 12:22:27 -0500

    Convert remaining nova tests to requests_mock
    Change-Id: Iff4341f2c83493c901f5e18570f82b0b5f7f3ad5

--- shade/tests/unit/test_usage.py
+++ shade/tests/unit/test_usage.py
@@ -12,21 +12,54 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import datetime
-import mock
+import uuid
 
-import shade
 from shade.tests.unit import base
 
 
 class TestUsage(base.RequestsMockTestCase):
 
-    @mock.patch.object(shade.OpenStackCloud, 'nova_client')
-    def test_get_usage(self, mock_nova):
+    def test_get_usage(self):
         project = self.mock_for_keystone_projects(project_count=1,
                                                   list_get=True)[0]
         start = end = datetime.datetime.now()
+
+        self.register_uris([
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     'compute', 'public',
+                     append=['os-simple-tenant-usage', project.project_id],
+                     qs_elements=[
+                         'start={now}'.format(now=start.isoformat()),
+                         'end={now}'.format(now=end.isoformat()),
+                     ]),
+                 json={"tenant_usage": {
+                     "server_usages": [
+                         {
+                             "ended_at": None,
+                             "flavor": "m1.tiny",
+                             "hours": 1.0,
+                             "instance_id": uuid.uuid4().hex,
+                             "local_gb": 1,
+                             "memory_mb": 512,
+                             "name": "instance-2",
+                             "started_at": "2012-10-08T20:10:44.541277",
+                             "state": "active",
+                             "tenant_id": "6f70656e737461636b20342065766572",
+                             "uptime": 3600,
+                             "vcpus": 1
+                         }
+                     ],
+                     "start": "2012-10-08T20:10:44.587336",
+                     "stop": "2012-10-08T21:10:44.587336",
+                     "tenant_id": "6f70656e737461636b20342065766572",
+                     "total_hours": 1.0,
+                     "total_local_gb_usage": 1.0,
+                     "total_memory_mb_usage": 512.0,
+                     "total_vcpus_usage": 1.0
+                 }})
+        ])
+
         self.op_cloud.get_compute_usage(project.project_id, start, end)
 
-        mock_nova.usage.get.assert_called_once_with(
-            start=start, end=end, tenant_id=project.project_id)
         self.assert_calls()
