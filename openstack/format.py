c9427165d90d4af2305b9c08333fa24c962988e8
Woo Cubic <iampurse@vip.qq.com>
2017-06-24 23:06:14 +0800

    make format de/serialize skip when value type is expected

--- openstack/format.py
+++ openstack/format.py
@@ -9,6 +9,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
+import six
 
 
 class Formatter(object):
@@ -27,6 +28,8 @@ class BoolStr(Formatter):
     @classmethod
     def deserialize(cls, value):
         """Convert a boolean string to a boolean"""
+        if isinstance(value, bool):
+            return value
         expr = str(value).lower()
         if "true" == expr:
             return True
@@ -39,11 +42,44 @@ class BoolStr(Formatter):
     @classmethod
     def serialize(cls, value):
         """Convert a boolean to a boolean string"""
+        if isinstance(value, six.string_types):
+            if str(value).lower() in ["true", "false"]:
+                return str(value).lower()
         if isinstance(value, bool):
             if value:
                 return "true"
             else:
                 return "false"
+        else:
+            raise ValueError("Unable to serialize boolean string: %s"
+                             % value)
+
+
+class YNBool(Formatter):
+    @classmethod
+    def deserialize(cls, value):
+        """Convert a boolean string to a boolean"""
+        if isinstance(value, bool):
+            return value
+        expr = str(value).lower()
+        if "y" == expr:
+            return True
+        elif "n" == expr:
+            return False
+        else:
+            raise ValueError("Unable to deserialize boolean string: %s"
+                             % value)
+
+    @classmethod
+    def serialize(cls, value):
+        """Convert a boolean to a boolean string"""
+        if value in ["Y", "N", "y", "n"]:
+            return str(value).upper()
+        if isinstance(value, bool):
+            if value:
+                return "Y"
+            else:
+                return "N"
         else:
             raise ValueError("Unable to serialize boolean string: %s"
                              % value)
