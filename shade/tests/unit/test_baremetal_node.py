b817f47782b05416c7b14ae67d0786b1e380d134
Julia Kreger <juliaashleykreger@gmail.com>
2017-09-08 18:53:52 +0000

    Migrate machine tests related to state transitions
    Migrating the machine tests where state transitions
    occur, such as turning power on/off, maintenance mode,
    hardware inspection, and general provision state changes
    such as activating a node and deactivating a node.
    
    Change-Id: I88663fecb79247d5473c47847f9c7405f447a80d

--- shade/tests/unit/test_baremetal_node.py
+++ shade/tests/unit/test_baremetal_node.py
@@ -486,6 +486,338 @@ class TestBaremetalNode(base.IronicTestCase):
 
         self.assert_calls()
 
+    def test_set_machine_maintenace_state(self):
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'maintenance']),
+                validate=dict(json={'reason': 'no reason'})),
+        ])
+        self.op_cloud.set_machine_maintenance_state(
+            self.fake_baremetal_node['uuid'], True, reason='no reason')
+
+        self.assert_calls()
+
+    def test_set_machine_maintenace_state_false(self):
+        self.register_uris([
+            dict(
+                method='DELETE',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'maintenance'])),
+        ])
+        self.op_cloud.set_machine_maintenance_state(
+            self.fake_baremetal_node['uuid'], False)
+
+        self.assert_calls
+
+    def test_remove_machine_from_maintenance(self):
+        self.register_uris([
+            dict(
+                method='DELETE',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'maintenance'])),
+        ])
+        self.op_cloud.remove_machine_from_maintenance(
+            self.fake_baremetal_node['uuid'])
+
+        self.assert_calls()
+
+    def test_set_machine_power_on(self):
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'power']),
+                validate=dict(json={'target': 'power on'})),
+        ])
+        return_value = self.op_cloud.set_machine_power_on(
+            self.fake_baremetal_node['uuid'])
+        self.assertIsNone(return_value)
+
+        self.assert_calls()
+
+    def test_set_machine_power_off(self):
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'power']),
+                validate=dict(json={'target': 'power off'})),
+        ])
+        return_value = self.op_cloud.set_machine_power_off(
+            self.fake_baremetal_node['uuid'])
+        self.assertIsNone(return_value)
+
+        self.assert_calls()
+
+    def test_set_machine_power_reboot(self):
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'power']),
+                validate=dict(json={'target': 'rebooting'})),
+        ])
+        return_value = self.op_cloud.set_machine_power_reboot(
+            self.fake_baremetal_node['uuid'])
+        self.assertIsNone(return_value)
+
+        self.assert_calls()
+
+    def test_set_machine_power_reboot_failure(self):
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'power']),
+                status_code=400,
+                json={'error': 'invalid'},
+                validate=dict(json={'target': 'rebooting'})),
+        ])
+        self.assertRaises(exc.OpenStackCloudException,
+                          self.op_cloud.set_machine_power_reboot,
+                          self.fake_baremetal_node['uuid'])
+
+        self.assert_calls()
+
+    def test_node_set_provision_state(self):
+        deploy_node = self.fake_baremetal_node.copy()
+        deploy_node['provision_state'] = 'deploying'
+        active_node = self.fake_baremetal_node.copy()
+        active_node['provision_state'] = 'active'
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'active',
+                                    'configdrive': 'http://host/file'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+        ])
+        self.op_cloud.node_set_provision_state(
+            self.fake_baremetal_node['uuid'],
+            'active',
+            configdrive='http://host/file')
+
+        self.assert_calls()
+
+    def test_node_set_provision_state_wait_timeout(self):
+        deploy_node = self.fake_baremetal_node.copy()
+        deploy_node['provision_state'] = 'deploying'
+        active_node = self.fake_baremetal_node.copy()
+        active_node['provision_state'] = 'active'
+        self.fake_baremetal_node['provision_state'] = 'available'
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'active'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=deploy_node),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=active_node),
+        ])
+        return_value = self.op_cloud.node_set_provision_state(
+            self.fake_baremetal_node['uuid'],
+            'active',
+            wait=True)
+
+        self.assertEqual(active_node, return_value)
+        self.assert_calls()
+
+    def test_node_set_provision_state_wait_timeout_fails(self):
+        # Intentionally time out.
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'active'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+        ])
+
+        self.assertRaises(
+            exc.OpenStackCloudException,
+            self.op_cloud.node_set_provision_state,
+            self.fake_baremetal_node['uuid'],
+            'active',
+            wait=True,
+            timeout=0.001)
+
+        self.assert_calls()
+
+    def test_node_set_provision_state_wait_success(self):
+        self.fake_baremetal_node['provision_state'] = 'active'
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'active'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+        ])
+
+        return_value = self.op_cloud.node_set_provision_state(
+            self.fake_baremetal_node['uuid'],
+            'active',
+            wait=True)
+
+        self.assertEqual(self.fake_baremetal_node, return_value)
+        self.assert_calls()
+
+    def test_node_set_provision_state_wait_failure_cases(self):
+        self.fake_baremetal_node['provision_state'] = 'foo failed'
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'active'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+        ])
+
+        self.assertRaises(
+            exc.OpenStackCloudException,
+            self.op_cloud.node_set_provision_state,
+            self.fake_baremetal_node['uuid'],
+            'active',
+            wait=True,
+            timeout=300)
+
+        self.assert_calls()
+
+    def test_node_set_provision_state_wait_provide(self):
+        self.fake_baremetal_node['provision_state'] = 'manageable'
+        available_node = self.fake_baremetal_node.copy()
+        available_node['provision_state'] = 'available'
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'provide'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=available_node),
+        ])
+        return_value = self.op_cloud.node_set_provision_state(
+            self.fake_baremetal_node['uuid'],
+            'provide',
+            wait=True)
+
+        self.assertEqual(available_node, return_value)
+        self.assert_calls()
+
+    def test_activate_node(self):
+        self.fake_baremetal_node['provision_state'] = 'active'
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'active',
+                                    'configdrive': 'http://host/file'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+        ])
+        return_value = self.op_cloud.activate_node(
+            self.fake_baremetal_node['uuid'],
+            configdrive='http://host/file',
+            wait=True)
+
+        self.assertIsNone(return_value)
+        self.assert_calls()
+
+    def test_deactivate_node(self):
+        self.fake_baremetal_node['provision_state'] = 'available'
+        self.register_uris([
+            dict(
+                method='PUT',
+                uri=self.get_mock_url(
+                    resource='nodes',
+                    append=[self.fake_baremetal_node['uuid'],
+                            'states', 'provision']),
+                validate=dict(json={'target': 'deleted'})),
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     resource='nodes',
+                     append=[self.fake_baremetal_node['uuid']]),
+                 json=self.fake_baremetal_node),
+        ])
+        return_value = self.op_cloud.deactivate_node(
+            self.fake_baremetal_node['uuid'],
+            wait=True)
+
+        self.assertIsNone(return_value)
+        self.assert_calls()
+
     def _test_update_machine(self, fake_node, field_name, changed=True):
         # The model has evolved over time, create the field if
         # we don't already have it.
