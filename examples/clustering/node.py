7803466ed2b12502a4e16bbe4f1c2078a0198646
Yuanbin.Chen <cybing4@gmail.com>
2018-01-23 17:35:47 +0800

    Add clustering guides node file, examples node code
    Change-Id: I905c424f86ef1f838ad2b637cb623f9ce7025466
    Signed-off-by: Yuanbin.Chen <cybing4@gmail.com>

---
+++ examples/clustering/node.py
@@ -0,0 +1,93 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Managing policies in the Cluster service.
+
+For a full guide see
+https://developer.openstack.org/sdks/python/openstacksdk/user/guides/cluster.html
+"""
+
+NODE_NAME = 'Test_Node'
+NODE_ID = 'dd803d4a-015d-4223-b15f-db29bad3146c'
+PROFILE_ID = "b0e3a680-e270-4eb8-9361-e5c9503fba0a"
+
+
+def list_nodes(conn):
+    print("List Nodes:")
+
+    for node in conn.clustering.nodes():
+        print(node.to_dict())
+    for node in conn.clustering.nodes(sort='asc:name'):
+        print(node.to_dict())
+
+
+def create_node(conn):
+    print("Create Node:")
+
+    spec = {
+        'name': NODE_NAME,
+        'profile_id': PROFILE_ID,
+    }
+    node = conn.clustering.create_node(**spec)
+    print(node.to_dict())
+
+
+def get_node(conn):
+    print("Get Node:")
+
+    node = conn.clustering.get_node(NODE_ID)
+    print(node.to_dict())
+
+
+def find_node(conn):
+    print("Find Node:")
+
+    node = conn.clustering.find_node(NODE_ID)
+    print(node.to_dict())
+
+
+def update_node(conn):
+    print("Update Node:")
+
+    spec = {
+        'name': 'Test_Node01',
+        'profile_id': 'c0e3a680-e270-4eb8-9361-e5c9503fba0b',
+    }
+
+    node = conn.clustering.update_node(NODE_ID, **spec)
+    print(node.to_dict())
+
+
+def delete_node(conn):
+    print("Delete Node:")
+
+    conn.clustering.delete_node(NODE_ID)
+    print("Node deleted.")
+    # node support force delete
+    conn.clustering.delete_node(NODE_ID, False, True)
+    print("Node deleted")
+
+
+def check_node(conn):
+    print("Check Node:")
+
+    node = conn.clustering.check_node(NODE_ID)
+    print(node.to_dict())
+
+
+def recover_node(conn):
+    print("Recover Node:")
+
+    spec = {'check': True}
+    node = conn.clustering.recover_node(NODE_ID, **spec)
+    print(node.to_dict())
