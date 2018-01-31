a65b18eab0aa6db51ec365801d2cd5b076e65647
Adrian Turjak <adriant@catalyst.net.nz>
2018-02-01 11:50:14 +1300

    Add server-side names to query param checking
    Rework the query params checking to include server-side names,
    as well as move to dedicated function on QueryParameters.
    
    Change transpose logic to include server-side names as fallback,
    with precedence for client-side names.
    
    Add tests for these changes, and fix missing assertEqual in old
    query mapping test.
    
    Change-Id: I3705075a0ea2911a06619a6e4dd13233c725887e
    Closes-bug: #1746535

--- openstack/tests/unit/test_resource.py
+++ openstack/tests/unit/test_resource.py
@@ -1285,7 +1285,9 @@ class TestResourceActions(base.TestCase):
 
         # Look at the `params` argument to each of the get calls that
         # were made.
-        self.session.get.call_args_list[0][1]["params"] = {qp_name: qp}
+        self.assertEqual(
+            self.session.get.call_args_list[0][1]["params"],
+            {qp_name: qp})
 
         self.assertEqual(self.session.get.call_args_list[0][0][0],
                          Test.base_path % {"something": uri_param})
@@ -1313,6 +1315,81 @@ class TestResourceActions(base.TestCase):
             self.assertFail('The above line should fail')
         except exceptions.InvalidResourceQuery as err:
             self.assertEqual(str(err), 'Invalid query params: something_wrong')
+
+    def test_values_as_list_params(self):
+        id = 1
+        qp = "query param!"
+        qp_name = "query-param"
+        uri_param = "uri param!"
+
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.links = {}
+        mock_response.json.return_value = {"resources": [{"id": id}]}
+
+        mock_empty = mock.Mock()
+        mock_empty.status_code = 200
+        mock_empty.links = {}
+        mock_empty.json.return_value = {"resources": []}
+
+        self.session.get.side_effect = [mock_response, mock_empty]
+
+        class Test(self.test_class):
+            _query_mapping = resource.QueryParameters(query_param=qp_name)
+            base_path = "/%(something)s/blah"
+            something = resource.URI("something")
+
+        results = list(Test.list(self.session, paginated=True,
+                       something=uri_param, **{qp_name: qp}))
+
+        self.assertEqual(1, len(results))
+
+        # Look at the `params` argument to each of the get calls that
+        # were made.
+        self.assertEqual(
+            self.session.get.call_args_list[0][1]["params"],
+            {qp_name: qp})
+
+        self.assertEqual(self.session.get.call_args_list[0][0][0],
+                         Test.base_path % {"something": uri_param})
+
+    def test_values_as_list_params_precedence(self):
+        id = 1
+        qp = "query param!"
+        qp2 = "query param!!!!!"
+        qp_name = "query-param"
+        uri_param = "uri param!"
+
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.links = {}
+        mock_response.json.return_value = {"resources": [{"id": id}]}
+
+        mock_empty = mock.Mock()
+        mock_empty.status_code = 200
+        mock_empty.links = {}
+        mock_empty.json.return_value = {"resources": []}
+
+        self.session.get.side_effect = [mock_response, mock_empty]
+
+        class Test(self.test_class):
+            _query_mapping = resource.QueryParameters(query_param=qp_name)
+            base_path = "/%(something)s/blah"
+            something = resource.URI("something")
+
+        results = list(Test.list(self.session, paginated=True, query_param=qp2,
+                       something=uri_param, **{qp_name: qp}))
+
+        self.assertEqual(1, len(results))
+
+        # Look at the `params` argument to each of the get calls that
+        # were made.
+        self.assertEqual(
+            self.session.get.call_args_list[0][1]["params"],
+            {qp_name: qp2})
+
+        self.assertEqual(self.session.get.call_args_list[0][0][0],
+                         Test.base_path % {"something": uri_param})
 
     def test_list_multi_page_response_paginated(self):
         ids = [1, 2]
