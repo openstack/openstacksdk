4c49acd831e17a2eeeb2c77e1cff7648025a3490
Ryan Brady <rbrady@redhat.com>
2018-07-06 09:42:46 -0400

    Add support for static routes
    The networking API v2 specification, which is implemented by OpenStack
    Neutron, features an optional routes parameter - when updating a router
    PUT requests). Static routes are crucial for routers to handle traffic
    from subnets not directly connected to a router.  This patch adds the
    routes parameter to the OpenStackCloud.update_router method as a list of
    dictionaries with destination and nexthop parameters.
    
    Example:
    [
       {
          "destination": "179.24.1.0/24",
          "nexthop": "172.24.3.99"
        }
    ]
    
    Change-Id: I14205b6bb071d0b46967f29b6287f74d8796add8

--- openstack/tests/functional/cloud/test_router.py
+++ openstack/tests/functional/cloud/test_router.py
@@ -276,6 +276,31 @@ class TestRouter(base.BaseFunctionalTestCase):
         self.assertEqual(router['external_gateway_info'],
                          updated['external_gateway_info'])
 
+    def test_update_router_routes(self):
+        router = self._create_and_verify_advanced_router(
+            external_cidr=u'10.7.7.0/24')
+
+        routes = [{
+            "destination": "10.7.7.0/24",
+            "nexthop": "10.7.7.99"
+        }]
+
+        updated = self.operator_cloud.update_router(
+            router['id'], routes=routes)
+        self.assertIsNotNone(updated)
+
+        for field in EXPECTED_TOPLEVEL_FIELDS:
+            self.assertIn(field, updated)
+
+        # Name is the only change we expect
+        self.assertEqual(routes, updated['routes'])
+
+        # Validate nothing else changed
+        self.assertEqual(router['status'], updated['status'])
+        self.assertEqual(router['admin_state_up'], updated['admin_state_up'])
+        self.assertEqual(router['external_gateway_info'],
+                         updated['external_gateway_info'])
+
     def test_update_router_admin_state(self):
         router = self._create_and_verify_advanced_router(
             external_cidr=u'10.8.8.0/24')
