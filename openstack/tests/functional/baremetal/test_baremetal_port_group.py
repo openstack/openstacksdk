26456e210bf070de6394966f27c4571bd577a50f
Dmitry Tantsur <divius.inside@gmail.com>
2018-08-10 17:19:24 +0200

    Add simple create/show/delete functional tests for all baremetal resources
    Required adding support for min_microversion to the base test class.
    
    Change-Id: I0aeb2b5fcb4575938bfbb8d7ca9b29d1f77a2ee5
    (cherry picked from commit ac8df03fd1f26b1c17ea45eec808fd7e8633dfef)

---
+++ openstack/tests/functional/baremetal/test_baremetal_port_group.py
@@ -0,0 +1,48 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+from openstack import exceptions
+from openstack.tests.functional.baremetal import base
+
+
+class TestBareMetalPortGroup(base.BaseBaremetalTest):
+
+    min_microversion = '1.23'
+
+    def setUp(self):
+        super(TestBareMetalPortGroup, self).setUp()
+        self.node = self.create_node()
+
+    def test_port_group_create_get_delete(self):
+        port_group = self.create_port_group()
+
+        loaded = self.conn.baremetal.get_port_group(port_group.id)
+        self.assertEqual(loaded.id, port_group.id)
+
+        self.conn.baremetal.delete_port_group(port_group,
+                                              ignore_missing=False)
+        self.assertRaises(exceptions.NotFoundException,
+                          self.conn.baremetal.get_port_group, port_group.id)
+
+    def test_port_group_negative_non_existing(self):
+        uuid = "5c9dcd04-2073-49bc-9618-99ae634d8971"
+        self.assertRaises(exceptions.NotFoundException,
+                          self.conn.baremetal.get_port_group, uuid)
+        self.assertRaises(exceptions.NotFoundException,
+                          self.conn.baremetal.find_port_group, uuid,
+                          ignore_missing=False)
+        self.assertRaises(exceptions.NotFoundException,
+                          self.conn.baremetal.delete_port_group, uuid,
+                          ignore_missing=False)
+        self.assertIsNone(self.conn.baremetal.find_port_group(uuid))
+        self.assertIsNone(self.conn.baremetal.delete_port_group(uuid))
