3c0f44f62d1bdca3fecfda1ccd8a716a28f18881
Dmitry Tantsur <divius.inside@gmail.com>
2019-02-25 17:26:09 +0100

    baremetal: support network_data when building configdrive
    This change adds support for network_data.json as defined in:
    https://specs.openstack.org/openstack/nova-specs/specs/liberty/implemented/metadata-service-network-info.html
    
    Also make user_data options, since it is actually optional.
    
    Change-Id: I28b7152e47bb6648b63d9216fbcdad22c1649888
    Story: #2005083

--- openstack/tests/unit/baremetal/test_configdrive.py
+++ openstack/tests/unit/baremetal/test_configdrive.py
@@ -24,16 +24,27 @@ from openstack.baremetal import configdrive
 
 
 class TestPopulateDirectory(testtools.TestCase):
-    def _check(self, metadata, user_data=None):
-        with configdrive.populate_directory(metadata, user_data) as d:
+    def _check(self, metadata, user_data=None, network_data=None):
+        with configdrive.populate_directory(metadata,
+                                            user_data=user_data,
+                                            network_data=network_data) as d:
             for version in ('2012-08-10', 'latest'):
                 with open(os.path.join(d, 'openstack', version,
                                        'meta_data.json')) as fp:
                     actual_metadata = json.load(fp)
 
                 self.assertEqual(metadata, actual_metadata)
+                network_data_file = os.path.join(d, 'openstack', version,
+                                                 'network_data.json')
                 user_data_file = os.path.join(d, 'openstack', version,
                                               'user_data')
+
+                if network_data is None:
+                    self.assertFalse(os.path.exists(network_data_file))
+                else:
+                    with open(network_data_file) as fp:
+                        self.assertEqual(network_data, json.load(fp))
+
                 if user_data is None:
                     self.assertFalse(os.path.exists(user_data_file))
                 else:
@@ -48,6 +59,9 @@ class TestPopulateDirectory(testtools.TestCase):
 
     def test_with_user_data(self):
         self._check({'foo': 42}, b'I am user data')
+
+    def test_with_network_data(self):
+        self._check({'foo': 42}, network_data={'networks': {}})
 
 
 @mock.patch('subprocess.Popen', autospec=True)
