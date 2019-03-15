9fb79904a4c4e6fc3a035145cb4b71f86a6731e8
Daniel Speichert <Daniel_Speichert@comcast.com>
2019-03-15 16:15:18 -0400

    Deduplicate next-page URL's query params
    This avoid HTTP 414 caused by endless addition of query params onto
    the next-page's URL that already contains them in case where the number
    of pages is high.
    
    Change-Id: I4f89e8e4837bb7c08c841e50070541038a2d2cc2

--- openstack/tests/unit/test_resource.py
+++ openstack/tests/unit/test_resource.py
@@ -1534,6 +1534,53 @@ class TestResourceActions(base.TestCase):
         self.assertEqual(2, len(self.session.get.call_args_list))
         self.assertIsInstance(results[0], self.test_class)
 
+    def test_list_response_paginated_with_links_and_query(self):
+        q_limit = 1
+        ids = [1, 2]
+        mock_response = mock.Mock()
+        mock_response.status_code = 200
+        mock_response.links = {}
+        mock_response.json.side_effect = [
+            {
+                "resources": [{"id": ids[0]}],
+                "resources_links": [{
+                    "href": "https://example.com/next-url?limit=%d" % q_limit,
+                    "rel": "next",
+                }]
+            }, {
+                "resources": [{"id": ids[1]}],
+            }, {
+                "resources": [],
+            }]
+
+        self.session.get.return_value = mock_response
+
+        class Test(self.test_class):
+            _query_mapping = resource.QueryParameters("limit")
+
+        results = list(Test.list(self.session, paginated=True, limit=q_limit))
+
+        self.assertEqual(2, len(results))
+        self.assertEqual(ids[0], results[0].id)
+        self.assertEqual(ids[1], results[1].id)
+        self.assertEqual(
+            mock.call('base_path',
+                      headers={'Accept': 'application/json'}, params={
+                          'limit': q_limit,
+                      },
+                      microversion=None),
+            self.session.get.mock_calls[0])
+        self.assertEqual(
+            mock.call('https://example.com/next-url',
+                      headers={'Accept': 'application/json'}, params={
+                          'limit': [str(q_limit)],
+                      },
+                      microversion=None),
+            self.session.get.mock_calls[2])
+
+        self.assertEqual(3, len(self.session.get.call_args_list))
+        self.assertIsInstance(results[0], self.test_class)
+
     def test_list_response_paginated_with_microversions(self):
         class Test(resource.Resource):
             service = self.service_name
