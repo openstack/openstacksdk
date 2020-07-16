9ed64cc73f549cccd67401a73f5f18fb8fd2a83a
Artom Lifshitz <alifshit@redhat.com>
2020-07-16 14:09:29 -0400

    Add func test for compute microversion 2.3
    Adds an admin API functional test for the following fields in the show
    server details API:
    
    OS-EXT-SRV-ATTR:reservation_id
    OS-EXT-SRV-ATTR:launch_index
    OS-EXT-SRV-ATTR:ramdisk_id
    OS-EXT-SRV-ATTR:kernel_id
    OS-EXT-SRV-ATTR:hostname
    OS-EXT-SRV-ATTR:root_device_name
    OS-EXT-SRV-ATTR:userdata
    os-extended-volumes:volumes_attached.delete_on_termination
    
    Change-Id: I3eb48139300b75caa7bf7b6464c4c8080d72fa04
    Story: 2007929

--- openstack/tests/functional/compute/v2/test_server.py
+++ openstack/tests/functional/compute/v2/test_server.py
@@ -16,6 +16,52 @@ from openstack.tests.functional.compute import base as ft_base
 from openstack.tests.functional.network.v2 import test_network
 
 
+class TestServerAdmin(ft_base.BaseComputeTest):
+
+    def setUp(self):
+        super(TestServerAdmin, self).setUp()
+        self._set_operator_cloud(interface='admin')
+        self.NAME = 'needstobeshortandlowercase'
+        self.USERDATA = 'SSdtIGFjdHVhbGx5IGEgZ29hdC4='
+        flavor = self.conn.compute.find_flavor(base.FLAVOR_NAME,
+                                               ignore_missing=False)
+        image = self.conn.compute.find_image(base.IMAGE_NAME,
+                                             ignore_missing=False)
+        volume = self.conn.create_volume(1)
+        sot = self.conn.compute.create_server(
+            name=self.NAME, flavor_id=flavor.id, image_id=image.id,
+            networks='none', user_data=self.USERDATA,
+            block_device_mapping=[{
+                'uuid': volume.id,
+                'source_type': 'volume',
+                'boot_index': 0,
+                'destination_type': 'volume',
+                'delete_on_termination': True,
+                'volume_size': 1}])
+        self.conn.compute.wait_for_server(sot, wait=self._wait_for_timeout)
+        assert isinstance(sot, server.Server)
+        self.assertEqual(self.NAME, sot.name)
+        self.server = sot
+
+    def tearDown(self):
+        sot = self.conn.compute.delete_server(self.server.id)
+        self.conn.compute.wait_for_delete(self.server,
+                                          wait=self._wait_for_timeout)
+        self.assertIsNone(sot)
+        super(TestServerAdmin, self).tearDown()
+
+    def test_get(self):
+        sot = self.conn.compute.get_server(self.server.id)
+        self.assertIsNotNone(sot.reservation_id)
+        self.assertIsNotNone(sot.launch_index)
+        self.assertIsNotNone(sot.ramdisk_id)
+        self.assertIsNotNone(sot.kernel_id)
+        self.assertEqual(self.NAME, sot.hostname)
+        self.assertTrue(sot.root_device_name.startswith('/dev'))
+        self.assertEqual(self.USERDATA, sot.user_data)
+        self.assertTrue(sot.attached_volumes[0]['delete_on_termination'])
+
+
 class TestServer(ft_base.BaseComputeTest):
 
     def setUp(self):
