7f6fbb27ca06eebc4ea1c5d564eb81a3ffb78332
John Petrini <john.d.petrini@gmail.com>
2020-09-01 15:21:59 -0400

    Don't set list_type to dict for server groups.
    When server_groups is present for retrieved server, this ends up
    with the following exception [1]. So we should rather have
    regular list instead of dict.
    
    [1] http://paste.openstack.org/show/797758/
    
    Change-Id: I40c4c5241d02aa9d6e61a8064b48a91bde5e6e77
    Story: 2007710
    Task: 39843
    (cherry picked from commit 1c2a06350d615af6bef2615a94224ec49ac155b4)

--- openstack/tests/unit/compute/v2/test_server.py
+++ openstack/tests/unit/compute/v2/test_server.py
@@ -105,6 +105,7 @@ EXAMPLE = {
             'name': 'default'
         }
     ],
+    'server_groups': ['3caf4187-8010-491f-b6f5-a4a68a40371e'],
     'status': 'ACTIVE',
     'tags': [],
     'tenant_id': '6f70656e737461636b20342065766572',
@@ -207,6 +208,7 @@ class TestServer(base.TestCase):
         self.assertEqual(EXAMPLE['name'], sot.name)
         self.assertEqual(EXAMPLE['progress'], sot.progress)
         self.assertEqual(EXAMPLE['tenant_id'], sot.project_id)
+        self.assertEqual(EXAMPLE['server_groups'], sot.server_groups)
         self.assertEqual(EXAMPLE['status'], sot.status)
         self.assertEqual(EXAMPLE['updated'], sot.updated_at)
         self.assertEqual(EXAMPLE['user_id'], sot.user_id)
