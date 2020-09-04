4b7a10839171a625f9ebd87abdef04e611d7ddd6
Dmitry Tantsur <dtantsur@protonmail.com>
2020-09-04 10:34:39 +0200

    Fix a bogus error in config loader when using several args with dashes
    Ironic routinely uses arguments like:
    
     --driver-info key1=value1 --driver-info key2=value2
    
    This is currently broken because of the duplication detection logic.
    Fix it to only trigger when there are several *different* froms of
    the same argument.
    
    Change-Id: I326d168d22476b86ae986b471b7c424e153f7076

--- openstack/tests/unit/config/test_loader.py
+++ openstack/tests/unit/config/test_loader.py
@@ -17,6 +17,7 @@ import tempfile
 import textwrap
 
 from openstack.config import loader
+from openstack import exceptions
 from openstack.tests.unit.config import base
 
 FILES = {
@@ -115,3 +116,26 @@ class TestLoader(base.TestCase):
         path, result = loader.OpenStackConfig()._load_yaml_json_file(
             tested_files)
         self.assertEqual(None, path)
+
+
+class TestFixArgv(base.TestCase):
+    def test_no_changes(self):
+        argv = ['-a', '-b', '--long-arg', '--multi-value', 'key1=value1',
+                '--multi-value', 'key2=value2']
+        expected = argv[:]
+        loader._fix_argv(argv)
+        self.assertEqual(expected, argv)
+
+    def test_replace(self):
+        argv = ['-a', '-b', '--long-arg', '--multi_value', 'key1=value1',
+                '--multi_value', 'key2=value2']
+        expected = ['-a', '-b', '--long-arg', '--multi-value', 'key1=value1',
+                    '--multi-value', 'key2=value2']
+        loader._fix_argv(argv)
+        self.assertEqual(expected, argv)
+
+    def test_mix(self):
+        argv = ['-a', '-b', '--long-arg', '--multi_value', 'key1=value1',
+                '--multi-value', 'key2=value2']
+        self.assertRaises(exceptions.ConfigException,
+                          loader._fix_argv, argv)
