c3ce787389f916d9b7d0e5b7a453fbb652fd870e
Irina Pereyaslavskaya <irina.pereyaslavskaya@gmail.com>
2020-12-18 16:42:01 +0300

    New volume availability zone resource, new functional and unit tests
    Change-Id: I1863095c2c6d6d3e10ef19a5afe00e1526d52c43

---
+++ openstack/tests/functional/block_storage/v3/test_availability_zone.py
@@ -0,0 +1,25 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+
+from openstack.tests.functional import base
+
+
+class TestAvailabilityZone(base.BaseFunctionalTest):
+
+    def test_list(self):
+        availability_zones = list(self.conn.block_storage.availability_zones())
+        self.assertGreater(len(availability_zones), 0)
+
+        for az in availability_zones:
+            self.assertIsInstance(az.name, str)
+            self.assertIsInstance(az.state, dict)
---
+++ openstack/tests/unit/block_storage/v3/test_availability_zone.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.block_storage.v3 import availability_zone as az
+
+from openstack.tests.unit import base
+
+IDENTIFIER = 'IDENTIFIER'
+EXAMPLE = {
+    "id": IDENTIFIER,
+    "zoneState": {
+        "available": True
+    },
+    "zoneName": "zone1"
+}
+
+
+class TestAvailabilityZone(base.TestCase):
+
+    def test_basic(self):
+        sot = az.AvailabilityZone()
+        self.assertEqual('availabilityZoneInfo', sot.resources_key)
+        self.assertEqual('/os-availability-zone', sot.base_path)
+        self.assertTrue(sot.allow_list)
+
+    def test_make_it(self):
+        sot = az.AvailabilityZone(**EXAMPLE)
+        self.assertEqual(EXAMPLE['id'], sot.id)
+        self.assertEqual(EXAMPLE['zoneState'], sot.state)
+        self.assertEqual(EXAMPLE['zoneName'], sot.name)
