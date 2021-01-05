4b594833e65e4a8d52b9217b36adfef900328365
Sagi Shnaidman <sshnaidm@redhat.com>
2021-01-05 16:17:49 +0200

    Support roles 'name' in list_roles call
    Change-Id: Ia615288da30426c2f689daa3e5f88376aead1d3f

--- openstack/tests/unit/cloud/test_identity_roles.py
+++ openstack/tests/unit/cloud/test_identity_roles.py
@@ -54,6 +54,22 @@ class TestIdentityRoles(base.TestCase):
         self.cloud.list_roles()
         self.assert_calls()
 
+    def test_list_role_by_name(self):
+        role_data = self._get_role_data()
+        self.register_uris([
+            dict(method='GET',
+                 uri=self.get_mock_url(
+                     qs_elements=['name={0}'.format(role_data.role_name)]),
+                 status_code=200,
+                 json={'roles': [role_data.json_response['role']]})
+        ])
+        role = self.cloud.list_roles(name=role_data.role_name)[0]
+
+        self.assertIsNotNone(role)
+        self.assertThat(role.id, matchers.Equals(role_data.role_id))
+        self.assertThat(role.name, matchers.Equals(role_data.role_name))
+        self.assert_calls()
+
     def test_get_role_by_name(self):
         role_data = self._get_role_data()
         self.register_uris([
