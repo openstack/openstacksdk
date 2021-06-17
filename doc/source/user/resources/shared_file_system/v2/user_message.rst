4914d8d754e312a5ca8cc8a6f47a5a0d7d934498
Kafilat Adeleke <adelekekafilatadenike@gmail.com>
2021-06-17 18:01:39 +0000

    Add user message to shared file system
    Introduce User Message class with basic methods
    including list, delete, get to
    shared file system storage service.
    
    Change-Id: I0cc3586f6681eb0941294e0ef27edb4d7e1f409a

---
+++ doc/source/user/resources/shared_file_system/v2/user_message.rst
@@ -0,0 +1,13 @@
+openstack.shared_file_system.v2.user_message
+============================================
+
+.. automodule:: openstack.shared_file_system.v2.user_message
+
+The UserMessage Class
+---------------------
+
+The ``UserMessage`` class inherits from
+:class:`~openstack.resource.Resource`.
+
+.. autoclass:: openstack.shared_file_system.v2.user_message.UserMessage
+   :members:
--- openstack/tests/unit/shared_file_system/v2/test_proxy.py
+++ openstack/tests/unit/shared_file_system/v2/test_proxy.py
@@ -15,6 +15,7 @@ from unittest import mock
 from openstack.shared_file_system.v2 import _proxy
 from openstack.shared_file_system.v2 import share
 from openstack.shared_file_system.v2 import storage_pool
+from openstack.shared_file_system.v2 import user_message
 from openstack.tests.unit import test_proxy_base
 
 
@@ -79,3 +80,30 @@ class TestSharedFileSystemProxy(test_proxy_base.TestProxyBase):
             self.proxy.storage_pools, storage_pool.StoragePool,
             method_kwargs={"details": False, "backend": "alpha"},
             expected_kwargs={"backend": "alpha"})
+
+
+class TestUserMessageProxy(test_proxy_base.TestProxyBase):
+
+    def setUp(self):
+        super(TestUserMessageProxy, self).setUp()
+        self.proxy = _proxy.Proxy(self.session)
+
+    def test_user_messages(self):
+        self.verify_list(self.proxy.user_messages, user_message.UserMessage)
+
+    def test_user_messages_queried(self):
+        self.verify_list(
+            self.proxy.user_messages, user_message.UserMessage,
+            method_kwargs={"action_id": "1"},
+            expected_kwargs={"action_id": "1"})
+
+    def test_user_message_get(self):
+        self.verify_get(self.proxy.get_user_message, user_message.UserMessage)
+
+    def test_delete_user_message(self):
+        self.verify_delete(
+            self.proxy.delete_user_message, user_message.UserMessage, False)
+
+    def test_delete_user_message_true(self):
+        self.verify_delete(
+            self.proxy.delete_user_message, user_message.UserMessage, True)
