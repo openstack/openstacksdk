a8c3cb29f29470529e5ebbd0c02ea8e34aca0b69
Artem Goncharov <Artem.goncharov@gmail.com>
2021-07-22 18:00:25 +0200

    Extend has_version function to accept version parameter
    We might need not only to check whether the service is supported, but
    also whether specific version of the service is working.
    Extend functional tests of the block-storage to actually check specific
    version.
    
    Change-Id: Ie71c4f818d5adc82466e06d994c258af271b5793
    (cherry picked from commit 0bc47cdb933e12fd142747371720e8a1f74cb795)

--- openstack/tests/functional/block_storage/v2/base.py
+++ openstack/tests/functional/block_storage/v2/base.py
@@ -22,5 +22,5 @@ class BaseBlockStorageTest(base.BaseFunctionalTest):
         self._set_user_cloud(block_storage_api_version='2')
         self._set_operator_cloud(block_storage_api_version='2')
 
-        if not self.user_cloud.has_service('block-storage'):
+        if not self.user_cloud.has_service('block-storage', '2'):
             self.skipTest('block-storage service not supported by cloud')
