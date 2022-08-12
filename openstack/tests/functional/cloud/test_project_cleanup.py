d9b7beff4373f3d35e1e791819265b8c732782c6
Artem Goncharov <artem.goncharov@gmail.com>
2022-08-12 15:07:49 +0200

    Fix incremental backups handling in project cleanup
    Cinder refuse to delete backup if there are dependent (incremental)
    backups. Introduce second iteration over backups to first drop all
    incremental ones and then all remainings.
    
    Story: 2010217
    Change-Id: Id4525bbbe11294b53e981e7654056eeb8969343d

--- openstack/tests/functional/cloud/test_project_cleanup.py
+++ openstack/tests/functional/cloud/test_project_cleanup.py
@@ -119,3 +119,84 @@ class TestProjectCleanup(base.BaseFunctionalTest):
         # Since we might not have enough privs to drop all nets - ensure
         # we do not have our known one
         self.assertNotIn(self.network_name, net_names)
+
+    def test_block_storage_cleanup(self):
+        if not self.user_cloud.has_service('object-store'):
+            self.skipTest('Object service is requred, but not available')
+
+        status_queue = queue.Queue()
+
+        vol = self.conn.block_storage.create_volume(name='vol1', size='1')
+        self.conn.block_storage.wait_for_status(vol)
+        s1 = self.conn.block_storage.create_snapshot(volume_id=vol.id)
+        self.conn.block_storage.wait_for_status(s1)
+        b1 = self.conn.block_storage.create_backup(volume_id=vol.id)
+        self.conn.block_storage.wait_for_status(b1)
+        b2 = self.conn.block_storage.create_backup(
+            volume_id=vol.id, is_incremental=True, snapshot_id=s1.id)
+        self.conn.block_storage.wait_for_status(b2)
+        b3 = self.conn.block_storage.create_backup(
+            volume_id=vol.id, is_incremental=True, snapshot_id=s1.id)
+        self.conn.block_storage.wait_for_status(b3)
+
+        # First round - check no resources are old enough
+        self.conn.project_cleanup(
+            dry_run=True,
+            wait_timeout=120,
+            status_queue=status_queue,
+            filters={'created_at': '2000-01-01'})
+
+        self.assertTrue(status_queue.empty())
+
+        # Second round - resource evaluation function return false, ensure
+        # nothing identified
+        self.conn.project_cleanup(
+            dry_run=True,
+            wait_timeout=120,
+            status_queue=status_queue,
+            filters={'created_at': '2200-01-01'},
+            resource_evaluation_fn=lambda x, y, z: False)
+
+        self.assertTrue(status_queue.empty())
+
+        # Third round - filters set too low
+        self.conn.project_cleanup(
+            dry_run=True,
+            wait_timeout=120,
+            status_queue=status_queue,
+            filters={'created_at': '2200-01-01'})
+
+        objects = []
+        while not status_queue.empty():
+            objects.append(status_queue.get())
+
+        # At least known networks should be identified
+        volumes = list(obj.id for obj in objects)
+        self.assertIn(vol.id, volumes)
+
+        # Fourth round - dry run with no filters, ensure everything identified
+        self.conn.project_cleanup(
+            dry_run=True,
+            wait_timeout=120,
+            status_queue=status_queue)
+
+        objects = []
+        while not status_queue.empty():
+            objects.append(status_queue.get())
+
+        vol_ids = list(obj.id for obj in objects)
+        self.assertIn(vol.id, vol_ids)
+
+        # Ensure network still exists
+        vol_check = self.conn.block_storage.get_volume(vol.id)
+        self.assertEqual(vol.name, vol_check.name)
+
+        # Last round - do a real cleanup
+        self.conn.project_cleanup(
+            dry_run=False,
+            wait_timeout=600,
+            status_queue=status_queue)
+
+        objects = []
+        while not status_queue.empty():
+            objects.append(status_queue.get())
