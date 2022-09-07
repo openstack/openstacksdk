169e27aa27af5d15a4a6f3a5d4ff7c74adc4d2c7
Stephen Finucane <stephenfin@redhat.com>
2022-09-07 13:03:07 +0100

    tests: Improve functional testing for image methods
    Improve test coverage for a number of APIs and split the large
    'test_image' file into multiple smaller files.
    
    Change-Id: I1bd3e115282b94a2b327fade7f46f1e83a9c30e1
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

---
+++ openstack/tests/functional/image/v2/test_schema.py
@@ -0,0 +1,37 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.image.v2 import schema as _schema
+from openstack.tests.functional.image.v2 import base
+
+
+class TestSchema(base.BaseImageTest):
+
+    def test_get_images_schema(self):
+        schema = self.conn.image.get_images_schema()
+        self.assertIsNotNone(schema)
+        self.assertIsInstance(schema, _schema.Schema)
+
+    def test_get_image_schema(self):
+        schema = self.conn.image.get_image_schema()
+        self.assertIsNotNone(schema)
+        self.assertIsInstance(schema, _schema.Schema)
+
+    def test_get_members_schema(self):
+        schema = self.conn.image.get_members_schema()
+        self.assertIsNotNone(schema)
+        self.assertIsInstance(schema, _schema.Schema)
+
+    def test_get_member_schema(self):
+        schema = self.conn.image.get_member_schema()
+        self.assertIsNotNone(schema)
+        self.assertIsInstance(schema, _schema.Schema)
---
+++ openstack/tests/functional/image/v2/test_task.py
@@ -0,0 +1,26 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from openstack.tests.functional.image.v2 import base
+
+
+class TestTask(base.BaseImageTest):
+
+    def test_tasks(self):
+        tasks = list(self.conn.image.tasks())
+        # NOTE(stephenfin): Yes, this is a dumb test. Basically all that we're
+        # checking is that the API endpoint is correct. It would be nice to
+        # have a proper check here that includes creation of tasks but we don't
+        # currently have the ability to do this and I'm not even sure if tasks
+        # are still really a supported thing. A potential future work item,
+        # perhaps.
+        self.assertIsInstance(tasks, list)
