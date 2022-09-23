70dbcfcd66d85875447a29a5abab110af38b6c5b
Artem Goncharov <artem.goncharov@gmail.com>
2022-09-23 10:19:04 +0200

    Initialize tests of real clouds
    We want to start testing sdk and osc on real clouds. They are all
    different, have different settings, policies, etc. In order to cope with
    that it would be necessary to rework lot of tests and code to ensure all
    of those "feautres" are properly detected by sdk and osc.
    
    Block-storage test base class should not set operator cloud, only tests
    that require it explicitly should use it
    
    Change-Id: I09d118097bade8f0d8ff497957846a754ebc7c1e

--- openstack/tests/functional/block_storage/v3/base.py
+++ openstack/tests/functional/block_storage/v3/base.py
@@ -20,7 +20,5 @@ class BaseBlockStorageTest(base.BaseFunctionalTest):
     def setUp(self):
         super(BaseBlockStorageTest, self).setUp()
         self._set_user_cloud(block_storage_api_version='3')
-        self._set_operator_cloud(block_storage_api_version='3')
-
         if not self.user_cloud.has_service('block-storage', '3'):
             self.skipTest('block-storage service not supported by cloud')
