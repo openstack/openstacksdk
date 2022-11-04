117642f1f9c66a0b0a9d742f967f416817f7d87e
Artem Goncharov <artem.goncharov@gmail.com>
2022-11-04 11:36:31 +0100

    Fix backup metadata management and update
    max_microversion on the backup resource was missed causing no
    microversion to be enabled at all for it. This leads to server refusing
    managing metadata of the backup. Fix this and add corresponding test
    with attempting to set metadata.
    Since this change sets microversion we also need to ensure we add all
    attributes of this version - encryption_key_id is the one added last.
    
    Change-Id: I2c161ca30524bf230104958b8afdd8cf0b682a89

--- openstack/tests/functional/block_storage/v3/test_backup.py
+++ openstack/tests/functional/block_storage/v3/test_backup.py
@@ -69,6 +69,21 @@ class TestBackup(base.BaseBlockStorageTest):
         self.assertEqual(self.BACKUP_NAME, sot.name)
         self.assertEqual(False, sot.is_incremental)
 
+    def test_create_metadata(self):
+        metadata_backup = self.user_cloud.block_storage.create_backup(
+            name=self.getUniqueString(),
+            volume_id=self.VOLUME_ID,
+            metadata=dict(foo="bar"))
+        self.user_cloud.block_storage.wait_for_status(
+            metadata_backup,
+            status='available',
+            failures=['error'],
+            interval=5,
+            wait=self._wait_for_timeout)
+        self.user_cloud.block_storage.delete_backup(
+            metadata_backup.id,
+            ignore_missing=False)
+
     def test_create_incremental(self):
         incremental_backup = self.user_cloud.block_storage.create_backup(
             name=self.getUniqueString(),
