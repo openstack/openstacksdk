41d19a5591eb11148fafb58cfbfc549324524f56
Stephen Finucane <stephenfin@redhat.com>
2023-07-27 10:54:11 +0100

    cloud: Remove unnecessary types import
    The 'openstack.resource2' module hasn't been a thing for a long time.
    
    Change-Id: I2cad44a0fb11f7d20af22da48f355432d213a7a0
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- openstack/cloud/_block_storage.py
+++ openstack/cloud/_block_storage.py
@@ -10,10 +10,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import types so that we can reference ListType in sphinx param declarations.
-# We can't just use list, because sphinx gets confused by
-# openstack.resource.Resource.list and openstack.resource2.Resource.list
-import types  # noqa
 import warnings
 
 from openstack.block_storage.v3._proxy import Proxy
