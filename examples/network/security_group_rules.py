4baf2eb2c391261ed44161329d8b0727b2ce8e33
Radoslaw Smigielski <rsmigiel@redhat.com>
2024-02-10 18:12:17 +0100

    Incorrect protocol type in create_security_group_rule()
    Example conn.network.create_security_group_rule() in examples/network/security_group_rules.py
    uses wrong protocol type protocol='HTTPS'. HTTPS is not allowed value,
    allowed values are: icmp, udp, tcp. It should be "protocol='tcp'" instead.
    
    Closes-Bug: #2052860
    Change-Id: I0fb0532ca562bcf93672a1ca3f1ce2e3ad864c6d

--- examples/network/security_group_rules.py
+++ examples/network/security_group_rules.py
@@ -31,7 +31,7 @@ def open_port(conn):
         security_group_id=example_sec_group.id,
         direction='ingress',
         remote_ip_prefix='0.0.0.0/0',
-        protocol='HTTPS',
+        protocol='tcp',
         port_range_max='443',
         port_range_min='443',
         ethertype='IPv4',
