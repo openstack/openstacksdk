4a8c12a80b883a314ff3bcbd2874fbed698b69ab
Stephen Finucane <stephenfin@redhat.com>
2024-06-26 13:52:25 +0100

    tests: Remove errant print
    Change-Id: Ic3adaa90d80093b3ae6f8227a6c218ef4174a830
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- openstack/tests/unit/test_utils.py
+++ openstack/tests/unit/test_utils.py
@@ -279,7 +279,6 @@ class TestTinyDAG(base.TestCase):
             for node in sot.walk(timeout=1):
                 executor.submit(test_walker_fn, sot, node, sorted_list)
         self._verify_order(sot.graph, sorted_list)
-        print(sorted_list)
         self.assertEqual(len(self.test_graph.keys()), len(sorted_list))
 
     def test_walk_raise(self):
