f6b9a3907a115e079a9a7d925a6950280805e8a6
Stephen Finucane <stephenfin@redhat.com>
2024-10-15 13:07:25 +0100

    compute: Add locked filter for Server
    Change-Id: If47890ec259249a9cdec4bb758f91889c7991f09
    Closes-bug: #2084547
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- openstack/compute/v2/server.py
+++ openstack/compute/v2/server.py
@@ -69,6 +69,7 @@ class Server(resource.Resource, metadata.MetadataMixin, tag.TagMixin):
         "key_name",
         "launch_index",
         "launched_at",
+        "locked",
         "locked_by",
         "name",
         "node",
