d3cc7e0960ffed202856ac094bf92492e90331af
Kim-Yukyung <yukyung116@gmail.com>
2025-10-17 01:17:17 +0900

    Add find and delete examples to Key Manager
    - Add find.py with find_secret() function for searching secrets
    - Add delete.py with delete_secret() function for deleting secrets
    - Update key_manager.rst documentation with new sections
    
    Change-Id: Id6db6bc2871700f04a636a66fb71fc0e3a5e7283
    Signed-off-by: Kim-Yukyung <yukyung116@gmail.com>

---
+++ examples/key_manager/delete.py
@@ -0,0 +1,29 @@
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""
+Delete resources from the Key Manager service.
+
+For a full guide see
+https://docs.openstack.org/openstacksdk/latest/user/guides/key_manager.html
+"""
+
+
+def delete_secret(conn, name_or_id):
+    print(f"Delete Secret: {name_or_id}")
+
+    secret = conn.key_manager.find_secret(name_or_id)
+
+    if secret:
+        conn.key_manager.delete_secret(secret)
+    else:
+        print("Secret not found")
