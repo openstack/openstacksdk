1c50568d2b781c22b4bfdb2ee4b0f70181bfdecc
Stephen Finucane <stephenfin@redhat.com>
2026-09-04 12:55:37 +0100

    typing: Resolve TODO
    The ksa fix [1] merged ages ago and our minimum has been bumped to a
    suitable version.
    
    [1] https://review.opendev.org/c/openstack/keystoneauth/+/951183
    
    Change-Id: I10a00711df937711e8ad5bf3b485fc6656d28bee
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- openstack/config/cloud_region.py
+++ openstack/config/cloud_region.py
@@ -1144,8 +1144,7 @@ class CloudRegion:
             session=self.get_session(),
             service_type=self.get_service_type(service_type),
             service_name=self.get_service_name(service_type),
-            # https://review.opendev.org/c/openstack/keystoneauth/+/951183
-            interface=self.get_interface(service_type),  # type: ignore
+            interface=self.get_interface(service_type),
             region_name=self.get_region_name(service_type),
         )
         endpoint = adapter.get_endpoint()
@@ -1248,8 +1247,7 @@ class CloudRegion:
             session=self.get_session(),
             service_type=self.get_service_type(service_type),
             service_name=self.get_service_name(service_type),
-            # https://review.opendev.org/c/openstack/keystoneauth/+/951183
-            interface=self.get_interface(service_type),  # type: ignore
+            interface=self.get_interface(service_type),
             version=version,
             min_version=min_api_version,
             max_version=max_api_version,
